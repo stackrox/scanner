e2c0873f0815bedd57d57eb5e188beb9eb85cd55
Connor Gorman <cgorman@stackrox.com>
2019-10-22 16:08:42 -0700

    Add Sanity test for OS packages in nginx:1.10

---
+++ pkg/clairify/client/client_test.go
@@ -0,0 +1,102 @@
+package client
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stackrox/scanner/pkg/clairify/fixtures"
+	"github.com/stackrox/scanner/pkg/clairify/types"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestClientSuite(t *testing.T) {
+	suite.Run(t, new(ClientTestSuite))
+}
+
+type ClientTestSuite struct {
+	suite.Suite
+
+	client *Clairify
+	server *httptest.Server
+}
+
+func (suite *ClientTestSuite) SetupSuite() {
+	masterRouter := http.NewServeMux()
+	masterRouter.HandleFunc("/clairify/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("{}"))
+	})
+	masterRouter.HandleFunc("/clairify/image/docker.io/library/nginx/1.10", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fixtures.GetLayerResponse))
+	})
+	masterRouter.HandleFunc("/clairify/image/docker.io/library/nginx/badtag", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fixtures.ErrorResponse))
+	})
+	masterRouter.HandleFunc("/clairify/sha/goodsha", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fixtures.GetLayerResponse))
+	})
+	masterRouter.HandleFunc("/clairify/sha/badsha", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fixtures.ErrorResponse))
+	})
+	masterRouter.HandleFunc("/clairify/image", func(w http.ResponseWriter, r *http.Request) {
+		if !strings.HasPrefix(r.Header.Get("Authorization"), "Basic") {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(fixtures.GetImageResponse))
+	})
+
+	suite.server = httptest.NewServer(masterRouter)
+	suite.client = New(suite.server.URL, true)
+}
+
+func (suite *ClientTestSuite) TearDownSuite() {
+	suite.server.Close()
+}
+
+func (suite *ClientTestSuite) TestPing() {
+	suite.NoError(suite.client.Ping())
+}
+
+func (suite *ClientTestSuite) TestAddImage() {
+	imageRequest := &types.ImageRequest{
+		Image:    "image",
+		Registry: "registry",
+	}
+	expectedImage := &types.Image{
+		SHA:      "sha",
+		Registry: "registry",
+		Remote:   "namespace/repo",
+		Tag:      "tag",
+	}
+	image, err := suite.client.AddImage("username", "password", imageRequest)
+	suite.NoError(err)
+	suite.Equal(expectedImage, image)
+}
+
+func (suite *ClientTestSuite) TestRetrieveImageDataBySHA() {
+	image := &types.Image{
+		Registry: "docker.io",
+		Remote:   "library/nginx",
+		Tag:      "1.10",
+	}
+	envelope, err := suite.client.RetrieveImageDataByName(image, true, true)
+	suite.NoError(err)
+	suite.NotNil(envelope)
+
+	image.Tag = "badtag"
+	envelope, err = suite.client.RetrieveImageDataByName(image, true, true)
+	suite.Error(err)
+}
+
+func (suite *ClientTestSuite) TestRetrieveImageDataByName() {
+	envelope, err := suite.client.RetrieveImageDataBySHA("goodsha", true, true)
+	suite.NoError(err)
+	suite.NotNil(envelope)
+
+	envelope, err = suite.client.RetrieveImageDataBySHA("badsha", true, true)
+	suite.Error(err)
+}
---
+++ tests/sanity_test.go
@@ -0,0 +1,100 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	v1 "github.com/stackrox/scanner/api/v1"
+	"github.com/stackrox/scanner/pkg/clairify/client"
+	"github.com/stackrox/scanner/pkg/clairify/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestImageSanity(t *testing.T) {
+	endpoint := os.Getenv("SCANNER_ENDPOINT")
+
+	cli := client.New(endpoint, true)
+
+	img, err := cli.AddImage("", "", &types.ImageRequest{
+		Image:    "nginx:1.10",
+		Registry: "https://registry-1.docker.io",
+		Insecure: false,
+	})
+	assert.NoError(t, err)
+
+	env, err := cli.RetrieveImageDataBySHA(img.SHA, true, true)
+	assert.NoError(t, err)
+	assert.Nil(t, env.Error)
+
+	expectedFeatures := []v1.Feature{
+		{
+			Name:            "diffutils",
+			NamespaceName:   "debian:8",
+			VersionFormat:   "dpkg",
+			Version:         "1:3.3-1",
+			Vulnerabilities: nil,
+			AddedBy:         "sha256:6d827a3ef358f4fa21ef8251f95492e667da826653fd43641cef5a877dc03a70",
+		},
+		{
+			Name:          "coreutils",
+			NamespaceName: "debian:8",
+			VersionFormat: "dpkg",
+			Version:       "8.23-4",
+			Vulnerabilities: []v1.Vulnerability{
+				{
+					Name:          "CVE-2016-2781",
+					NamespaceName: "debian:8",
+					Description:   "chroot in GNU coreutils, when used with --userspec, allows local users to escape to the parent session via a crafted TIOCSTI ioctl call, which pushes characters to the terminal's input buffer.",
+					Link:          "https://security-tracker.debian.org/tracker/CVE-2016-2781",
+					Severity:      "Low",
+					Metadata: map[string]interface{}{
+						"NVD": map[string]interface{}{
+							"CVSSv2": map[string]interface{}{
+								"ExploitabilityScore": 3.9,
+								"ImpactScore":         2.9,
+								"PublishedDateTime":   "2017-02-07T15:59Z",
+								"Score":               2.1,
+								"Vectors":             "AV:L/AC:L/Au:N/C:N/I:P/A:N",
+							},
+							"CVSSv3": map[string]interface{}{
+								"ExploitabilityScore": 2.0,
+								"ImpactScore":         4.0,
+								"Score":               6.5,
+								"Vectors":             "CVSS:3.0/AV:L/AC:L/PR:L/UI:N/S:C/C:N/I:H/A:N",
+							},
+						},
+					},
+				},
+				{
+					Name:          "CVE-2017-18018",
+					NamespaceName: "debian:8",
+					Description:   "In GNU Coreutils through 8.29, chown-core.c in chown and chgrp does not prevent replacement of a plain file with a symlink during use of the POSIX \"-R -L\" options, which allows local users to modify the ownership of arbitrary files by leveraging a race condition.",
+					Link:          "https://security-tracker.debian.org/tracker/CVE-2017-18018",
+					Severity:      "Negligible",
+					Metadata: map[string]interface{}{
+						"NVD": map[string]interface{}{
+							"CVSSv2": map[string]interface{}{
+								"ExploitabilityScore": 3.4,
+								"ImpactScore":         2.9,
+								"PublishedDateTime":   "2018-01-04T04:29Z",
+								"Score":               1.9,
+								"Vectors":             "AV:L/AC:M/Au:N/C:N/I:P/A:N",
+							},
+							"CVSSv3": map[string]interface{}{
+								"ExploitabilityScore": 1.0,
+								"ImpactScore":         3.6,
+								"Score":               4.7,
+								"Vectors":             "CVSS:3.0/AV:L/AC:H/PR:L/UI:N/S:U/C:N/I:H/A:N",
+							},
+						},
+					},
+				},
+			},
+			AddedBy: "sha256:6d827a3ef358f4fa21ef8251f95492e667da826653fd43641cef5a877dc03a70",
+		},
+	}
+
+	for _, feature := range expectedFeatures {
+		assert.Contains(t, env.Layer.Features, feature)
+	}
+}
