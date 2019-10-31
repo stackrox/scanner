7c11a9961062f527a759c3e9d86bec07cc648a44
Viswajith Venugopal <viswajithiii@gmail.com>
2019-10-31 12:25:58 -0700

    Fix

--- api/grpc/grpc.go
+++ api/grpc/grpc.go
@@ -150,7 +150,7 @@ func (a *apiImpl) muxer(localConn *grpc.ClientConn) http.Handler {
 
 func httpGrpcRouter(grpcServer *grpc.Server, httpHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-SourceType"), "application/grpc") {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
 		} else {
 			httpHandler.ServeHTTP(w, r)
