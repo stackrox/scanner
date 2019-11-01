38ca245871afa0d93d9501063d288f9d97740fbe
Viswajith Venugopal <viswajithiii@gmail.com>
2019-10-31 17:46:20 -0700

    Add test

--- tests/grpc_test.go
+++ tests/grpc_test.go
@@ -2,33 +2,17 @@ package tests
 
 import (
 	"context"
-	"crypto/tls"
 	"testing"
 
 	v1 "github.com/stackrox/scanner/generated/api/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 func TestGRPCScanImage(t *testing.T) {
-	clientTLSConfig := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-	conn, err := grpc.DialContext(context.Background(), "localhost:8081", grpc.WithTransportCredentials(credentials.NewTLS(clientTLSConfig)))
-	require.NoError(t, err)
-
+	conn := connectToScanner(t)
 	client := v1.NewScanServiceClient(conn)
-	scanImageResp, err := client.ScanImage(context.Background(), &v1.ScanImageRequest{
-		Image: "nginx",
-		Registry: &v1.ScanImageRequest_RegistryData{
-			Url:      "https://registry-1.docker.io",
-			Username: "",
-			Password: "",
-		},
-	})
-	require.NoError(t, err)
+	scanImageResp := scanPublicDockerHubImage(client, "nginx", t)
 
 	getScanResp, err := client.GetScan(context.Background(), &v1.GetScanRequest{
 		ImageSpec: &v1.ImageSpec{Image: scanImageResp.Image.GetImage()},
---
+++ tests/utils_test.go
@@ -0,0 +1,37 @@
+package tests
+
+import (
+	"context"
+	"crypto/tls"
+	"os"
+	"testing"
+
+	"github.com/stackrox/rox/pkg/stringutils"
+	v1 "github.com/stackrox/scanner/generated/api/v1"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func connectToScanner(t *testing.T) *grpc.ClientConn {
+	gRPCEndpoint := stringutils.OrDefault(os.Getenv("SCANNER_GRPC_ENDPOINT"), "localhost:8081")
+	clientTLSConfig := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	conn, err := grpc.DialContext(context.Background(), gRPCEndpoint, grpc.WithTransportCredentials(credentials.NewTLS(clientTLSConfig)))
+	require.NoError(t, err)
+	return conn
+}
+
+func scanPublicDockerHubImage(client v1.ScanServiceClient, imageName string, t *testing.T) *v1.ScanImageResponse {
+	scanImageResp, err := client.ScanImage(context.Background(), &v1.ScanImageRequest{
+		Image: imageName,
+		Registry: &v1.ScanImageRequest_RegistryData{
+			Url:      "https://registry-1.docker.io",
+		},
+	})
+	require.NoError(t, err)
+	require.Equal(t, scanImageResp.GetStatus(), v1.ScanStatus_SUCCEEDED)
+	return scanImageResp
+}
+
