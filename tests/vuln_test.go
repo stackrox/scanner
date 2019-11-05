f2fcab14f99c39ccd89fff040d590125777149cd
Viswajith Venugopal <viswajithiii@gmail.com>
2019-11-04 21:59:34 -0800

    Fix?

--- tests/vuln_test.go
+++ tests/vuln_test.go
@@ -18,29 +18,34 @@ func TestVulns(t *testing.T) {
 		ImageSpec: scanResp.GetImage(),
 	})
 	require.NoError(t, err)
+
+	// If the test failed, print helpful debug information.
+	defer func() {
+		if t.Failed() {
+			for _, feat := range scan.GetImage().GetFeatures() {
+				fmt.Println(feat.GetName(), feat.GetVersion())
+			}
+			fmt.Println("DONE PRINTING COMPONENTS FROM SCAN")
+
+			componentsMap, err := client.GetLanguageLevelComponents(context.Background(), &v1.GetLanguageLevelComponentsRequest{
+				ImageSpec: scanResp.GetImage(),
+			})
+			require.NoError(t, err)
+			for _, components := range componentsMap.GetLayerToComponents() {
+				for _, component := range components.GetComponents() {
+					fmt.Println(component.GetName(), component.GetVersion())
+				}
+			}
+			fmt.Println("DONE PRINTING LANGUAGE LEVEL COMPONENTS")
+		}
+	}()
 	expectedFeatures := []struct {
 		name          string
 		version       string
 		expectedVulns []string
 	}{
-		{"Django", "2.1", []string{"CVE-2019-14235"}},
+		{"django", "2.1", []string{"CVE-2019-14235"}},
 	}
-
-	for _, feat := range scan.GetImage().GetFeatures() {
-		fmt.Println(feat.GetName(), feat.GetVersion())
-	}
-	fmt.Println("DONE PRINTING COMPONENTS FROM SCAN")
-
-	componentsMap, err := client.GetLanguageLevelComponents(context.Background(), &v1.GetLanguageLevelComponentsRequest{
-		ImageSpec: scanResp.GetImage(),
-	})
-	require.NoError(t, err)
-	for _, components := range componentsMap.GetLayerToComponents() {
-		for _, component := range components.GetComponents() {
-			fmt.Println(component.GetName(), component.GetVersion())
-		}
-	}
-	fmt.Println("DONE PRINTING COMPONENTS")
 
 	for _, expectedFeat := range expectedFeatures {
 		t.Run(fmt.Sprintf("%s/%s", expectedFeat.name, expectedFeat.version), func(t *testing.T) {
