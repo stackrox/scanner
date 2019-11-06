e8ae60d901f5d011b277c415b1c04d7b0aaa6d53
Viswajith Venugopal <viswajithiii@gmail.com>
2019-11-05 16:49:00 -0800

    Add test for NPM

--- tests/vuln_test.go
+++ tests/vuln_test.go
@@ -10,10 +10,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestVulns(t *testing.T) {
+type expectedFeature struct {
+	name          string
+	version       string
+	expectedVulns []string
+}
+
+type singleTestCase struct {
+	imageTag         string
+	expectedFeatures []expectedFeature
+}
+
+func testSingleVulnImage(testCase singleTestCase, t *testing.T) {
 	conn := connectToScanner(t)
 	client := v1.NewScanServiceClient(conn)
-	scanResp := scanDockerIOStackRoxImage(client, "docker.io/stackrox/vuln-images:django-cve-2019-14235", t)
+	scanResp := scanDockerIOStackRoxImage(client, fmt.Sprintf("docker.io/stackrox/vuln-images:%s", testCase.imageTag), t)
 	scan, err := client.GetScan(context.Background(), &v1.GetScanRequest{
 		ImageSpec: scanResp.GetImage(),
 	})
@@ -39,15 +50,7 @@ func TestVulns(t *testing.T) {
 			fmt.Println("DONE PRINTING LANGUAGE LEVEL COMPONENTS")
 		}
 	}()
-	expectedFeatures := []struct {
-		name          string
-		version       string
-		expectedVulns []string
-	}{
-		{"django", "2.1", []string{"CVE-2019-14235"}},
-	}
-
-	for _, expectedFeat := range expectedFeatures {
+	for _, expectedFeat := range testCase.expectedFeatures {
 		t.Run(fmt.Sprintf("%s/%s", expectedFeat.name, expectedFeat.version), func(t *testing.T) {
 			var matching *v1.Feature
 			for _, actualFeature := range scan.GetImage().GetFeatures() {
@@ -67,6 +70,23 @@ func TestVulns(t *testing.T) {
 				}
 				assert.True(t, found, "Vuln %s not found", expectedVuln)
 			}
+		})
+	}
+}
+
+// This test tests vulnerable images pushed up to docker.io/stackrox/vuln-images.
+// Images are pushed from https://github.com/stackrox/vuln-images.
+func TestStackroxVulnImages(t *testing.T) {
+	for _, testCase := range []singleTestCase{
+		{
+			imageTag: "django-cve-2019-14235",
+			expectedFeatures: []expectedFeature{
+				{"django", "2.1", []string{"CVE-2019-14235"}},
+			},
+		},
+	} {
+		t.Run(testCase.imageTag, func(t *testing.T) {
+			testSingleVulnImage(testCase, t)
 		})
 	}
 }
