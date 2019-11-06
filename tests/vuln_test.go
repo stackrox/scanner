0a5d714273c14b8b5161a83947d1dc8b563829e4
Viswajith Venugopal <viswajithiii@gmail.com>
2019-11-06 11:19:44 -0800

    Add test for the vulnerable lodash image (#46)

--- tests/vuln_test.go
+++ tests/vuln_test.go
@@ -11,10 +11,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestVulns(t *testing.T) {
+type expectedFeature struct {
+	name          string
+	version       string
+	expectedVulns []string
+}
+
+type singleTestCase struct {
+	imageTag         string
+	expectedFeatures []expectedFeature
+}
+
+func testSingleVulnImage(testCase singleTestCase, t *testing.T) {
 	conn := connectToScanner(t)
 	client := v1.NewScanServiceClient(conn)
-	scanResp := scanDockerIOStackRoxImage(client, "docker.io/stackrox/vuln-images:django-cve-2019-14235", t)
+	scanResp := scanDockerIOStackRoxImage(client, fmt.Sprintf("docker.io/stackrox/vuln-images:%s", testCase.imageTag), t)
 	scan, err := client.GetScan(context.Background(), &v1.GetScanRequest{
 		ImageSpec: scanResp.GetImage(),
 	})
@@ -40,15 +51,7 @@ func TestVulns(t *testing.T) {
 			fmt.Println("DONE PRINTING LANGUAGE LEVEL COMPONENTS")
 		}
 	}()
-	expectedFeatures := []struct {
-		name          string
-		version       string
-		expectedVulns []string
-	}{
-		{"django", "2.1", []string{"CVE-2019-14235"}},
-	}
-
-	for _, expectedFeat := range expectedFeatures {
+	for _, expectedFeat := range testCase.expectedFeatures {
 		t.Run(fmt.Sprintf("%s/%s", expectedFeat.name, expectedFeat.version), func(t *testing.T) {
 			matchingIdx := sliceutils.FindMatching(scan.GetImage().GetFeatures(), func(feature *v1.Feature) bool {
 				return feature.GetName() == expectedFeat.name && feature.GetVersion() == expectedFeat.version
@@ -62,6 +65,29 @@ func TestVulns(t *testing.T) {
 				})
 				assert.NotEqual(t, -1, matchingIdx, "Vuln %s not found", expectedVuln)
 			}
+		})
+	}
+}
+
+// This test tests vulnerable images pushed up to docker.io/stackrox/vuln-images.
+// Images are pushed from https://github.com/stackrox/vuln-images.
+func TestStackroxVulnImages(t *testing.T) {
+	for _, testCase := range []singleTestCase{
+		{
+			imageTag: "django-cve-2019-14235",
+			expectedFeatures: []expectedFeature{
+				{"django", "2.1", []string{"CVE-2019-14235"}},
+			},
+		},
+		{
+			imageTag: "lodash-cve-2019-1010266",
+			expectedFeatures: []expectedFeature{
+				{"lodash", "4.17.10", []string{"CVE-2019-1010266"}},
+			},
+		},
+	} {
+		t.Run(testCase.imageTag, func(t *testing.T) {
+			testSingleVulnImage(testCase, t)
 		})
 	}
 }
