53b914c0adbe71698b863b163da744470936beaf
Malte Isberner <mi@stackrox.com>
2019-11-27 07:51:36 +0100

    remove proxy init logic from updater main

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/stackrox/rox/pkg/httputil/proxy"
 	"github.com/stackrox/scanner/cmd/updater/diffdumps"
 	"github.com/stackrox/scanner/cmd/updater/generatedump"
 	"github.com/stackrox/scanner/cmd/updater/loaddump"
@@ -15,18 +13,7 @@ import (
 	_ "github.com/stackrox/scanner/ext/vulnsrc/all"
 )
 
-const (
-	proxyConfigPath = "/run/secrets/stackrox.io/proxy-config"
-	proxyConfigFile = "config.yaml"
-)
-
-func init() {
-	proxy.UseWithDefaultTransport()
-}
-
 func main() {
-	proxy.WatchProxyConfig(context.Background(), proxyConfigPath, proxyConfigFile, true)
-
 	c := &cobra.Command{
 		Short:        "Commands related to fetching updated vulnerability definitions",
 		SilenceUsage: true,
