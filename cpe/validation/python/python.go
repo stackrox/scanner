4aeec91ecf04a6a6b568b74a4b4178f58d12bdc4
Connor Gorman <cgorman@stackrox.com>
2019-12-02 13:53:42 -0800

    relax python validation

--- cpe/validation/python/python.go
+++ cpe/validation/python/python.go
@@ -4,9 +4,14 @@ import (
 	"strings"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/stackrox/rox/pkg/set"
 	"github.com/stackrox/scanner/cpe/match"
 	"github.com/stackrox/scanner/cpe/validation"
 	"github.com/stackrox/scanner/pkg/component"
+)
+
+var (
+	knownPkgs = set.NewFrozenStringSet("ansible", "jinja2")
 )
 
 type validator struct{}
@@ -15,13 +20,19 @@ func (v validator) ValidateResult(result match.Result) bool {
 	if result.CPE.Vendor != "" {
 		return true
 	}
-	if strings.Contains(result.CPE.Vendor, "python") {
+	if knownPkgs.Contains(result.CPE.Product) {
 		return true
 	}
-	if strings.Contains(result.CPE.Product, "python") {
+	if strings.Contains(result.CPE.Vendor, "py") {
 		return true
 	}
-	log.Debugf("Python %s: %s failed validation", result.CPE.Product, result.CVE.ID())
+	if strings.Contains(result.CPE.Product, "py") {
+		return true
+	}
+	if strings.Contains(strings.ToLower(result.Vuln.Description), "py") {
+		return true
+	}
+	log.Debugf("Python (%s %s): %s failed validation: %v", result.CPE.Vendor, result.CPE.Product, result.CVE.ID(), result.Vuln.Description)
 	return false
 }
 
