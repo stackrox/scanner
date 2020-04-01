e85b8250a1d18477201a1fbb45a40e0084f47035
Viswajith Venugopal <viswajithiii@gmail.com>
2020-03-31 22:10:38 -0700

    WIP

--- .circleci/create-dev-license-update-pr.sh
+++ .circleci/create-dev-license-update-pr.sh
@@ -23,7 +23,7 @@ curl -sS --fail \
 	-o "$pr_response_file" \
 	-X POST \
 	-H "Authorization: token ${GITHUB_TOKEN}" \
-	'https://api.github.com/repos/stackrox/rox/pulls' \
+	'https://api.github.com/repos/stackrox/scanner/pulls' \
 	-d"{
 	\"title\": \"Automatic update of dev license ($(date '+%Y-%m-%d'))\",
 	\"body\": $(jq -sR <<<"$message"),
@@ -36,7 +36,7 @@ pr_number="$(jq <"$pr_response_file" -r '.number')"
 curl -sS --fail \
 	-X POST \
 	-H "Authorization: token ${GITHUB_TOKEN}" \
-	"https://api.github.com/repos/stackrox/rox/pulls/${pr_number}/requested_reviewers" \
+	"https://api.github.com/repos/stackrox/scanner/pulls/${pr_number}/requested_reviewers" \
 	-d'{
 	"team_reviewers": ["dev-license-update-approvers"]
 }'
