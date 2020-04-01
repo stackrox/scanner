e6e207bab353a7394b5dc2fdbb75080a90eace53
Viswajith Venugopal <viswajithiii@gmail.com>
2020-04-01 14:50:28 -0700

    Write automation to sync dev license with rox (#141)

---
+++ .circleci/create-dev-license-update-pr.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+[[ -n "${GITHUB_TOKEN}" ]] || { echo >&2 "No GitHub token found"; exit 2; }
+
+usage() {
+  echo >&2 "Usage: $0 <label_name>"
+  exit 2
+}
+
+branch_name="$1"
+
+[[ -n "$branch_name" ]] || usage
+
+pr_response_file="$(mktemp)"
+
+message="Hello,
+it is time to sync our development license with the one from rox.
+Once all CI checks pass on this PR, please approve and merge it."
+
+curl -sS --fail \
+	-o "$pr_response_file" \
+	-X POST \
+	-H "Authorization: token ${GITHUB_TOKEN}" \
+	'https://api.github.com/repos/stackrox/scanner/pulls' \
+	-d"{
+	\"title\": \"Automatic update of dev license ($(date '+%Y-%m-%d'))\",
+	\"body\": $(jq -sR <<<"$message"),
+	\"head\": \"${branch_name}\",
+	\"base\": \"master\"
+}"
+
+pr_number="$(jq <"$pr_response_file" -r '.number')"
+
+curl -sS --fail \
+	-X POST \
+	-H "Authorization: token ${GITHUB_TOKEN}" \
+	"https://api.github.com/repos/stackrox/scanner/pulls/${pr_number}/requested_reviewers" \
+	-d'{
+	"reviewers": ["viswajithiii", "connorgorman"]
+}'
