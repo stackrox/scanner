b752aa8efe44d3b2fe54e947200ecc0b7c454b79
RTann <ross@stackrox.com>
2020-06-24 17:11:23 -0700

    Upsert KeyValue

--- database/pgsql/keyvalue.go
+++ database/pgsql/keyvalue.go
@@ -23,7 +23,7 @@ import (
 )
 
 // InsertKeyValue stores (or updates) a single key / value tuple.
-func (pgSQL *pgSQL) InsertKeyValue(key, value string) (err error) {
+func (pgSQL *pgSQL) InsertKeyValue(key, value string) error {
 	if key == "" || value == "" {
 		log.Warning("could not insert a flag which has an empty name or value")
 		return commonerr.NewBadRequestError("could not insert a flag which has an empty name or value")
@@ -31,39 +31,12 @@ func (pgSQL *pgSQL) InsertKeyValue(key, value string) (err error) {
 
 	defer observeQueryTime("InsertKeyValue", "all", time.Now())
 
-	// Upsert.
-	//
-	// Note: UPSERT works only on >= PostgreSQL 9.5 which is not yet supported by AWS RDS.
-	//       The best solution is currently the use of http://dba.stackexchange.com/a/13477
-	//       but the key/value storage doesn't need to be super-efficient and super-safe at the
-	//       moment so we can just use a client-side solution with transactions, based on
-	//       http://postgresql.org/docs/current/static/plpgsql-control-structures.html.
-	// TODO(Quentin-M): Enable Upsert as soon as 9.5 is stable.
-
-	for {
-		// First, try to update.
-		r, err := pgSQL.Exec(updateKeyValue, value, key)
-		if err != nil {
-			return handleError("updateKeyValue", err)
-		}
-		if n, _ := r.RowsAffected(); n > 0 {
-			// Updated successfully.
-			return nil
-		}
-
-		// Try to insert the key.
-		// If someone else inserts the same key concurrently, we could get a unique-key violation error.
-		_, err = pgSQL.Exec(insertKeyValue, key, value)
-		if err != nil {
-			if isErrUniqueViolation(err) {
-				// Got unique constraint violation, retry.
-				continue
-			}
-			return handleError("insertKeyValue", err)
-		}
-
-		return nil
+	_, err := pgSQL.Exec(upsertKeyValue, value, key)
+	if err != nil {
+		return handleError("upsertKeyValue", err)
 	}
+
+	return nil
 }
 
 // GetValue reads a single key / value tuple and returns an empty string if the key doesn't exist.
