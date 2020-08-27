d636b8b17e23a8bc33c3d93fc23d276f5e016665
Connor Gorman <connorgorman@users.noreply.github.com>
2020-08-27 12:23:41 -0700

    ROX-5269: Make the getMostSpecificCPE function have deterministic output (#207)

--- cpe/cpe.go
+++ cpe/cpe.go
@@ -102,21 +102,26 @@ func getAttributes(c *component.Component) []*wfn.Attributes {
 	return attrs
 }
 
+func compareAttributes(c1, c2 wfn.AttributesWithFixedIn) int {
+	if cmp := strings.Compare(c1.Vendor, c2.Vendor); cmp != 0 {
+		return cmp
+	}
+	if cmp := strings.Compare(c1.Product, c2.Product); cmp != 0 {
+		return cmp
+	}
+	return strings.Compare(c1.Version, c2.Version)
+}
+
+// getMostSpecificCPE deterministically returns the CPE that is the most specific
+// from the set of matches. This function requires that len(cpes) > 0
 func getMostSpecificCPE(cpes []wfn.AttributesWithFixedIn) wfn.AttributesWithFixedIn {
-	max := cpes[0]
-	for _, c := range cpes {
-		if max.Vendor != "" && c.Vendor == "" {
-			continue
-		}
-		if max.Vendor == "" && c.Vendor != "" {
-			max = c
-			continue
-		}
-		if len(c.Version) > len(max.Version) {
-			max = c
+	mostSpecificCPE := cpes[0]
+	for _, cpe := range cpes[1:] {
+		if compareAttributes(cpe, mostSpecificCPE) > 0 {
+			mostSpecificCPE = cpe
 		}
 	}
-	return max
+	return mostSpecificCPE
 }
 
 func CheckForVulnerabilities(layer string, components []*component.Component) []database.FeatureVersion {
