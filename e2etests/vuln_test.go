1f58e5d48061605e34ae530ec6e3c414952531bc
Connor Gorman <cgorman@stackrox.com>
2020-10-26 10:38:44 -0700

    lotsa tests

--- e2etests/vuln_test.go
+++ e2etests/vuln_test.go
@@ -24,7 +24,7 @@ type feature struct {
 }
 
 type singleTestCase struct {
-	imageTag         string
+	image            string
 	expectedFeatures []feature
 	unexpectedVulns  []feature
 }
@@ -32,7 +32,7 @@ type singleTestCase struct {
 func testSingleVulnImage(testCase singleTestCase, t *testing.T) {
 	conn := connectToScanner(t)
 	client := v1.NewScanServiceClient(conn)
-	scanResp := scanDockerIOStackRoxImage(client, fmt.Sprintf("docker.io/stackrox/vuln-images:%s", testCase.imageTag), t)
+	scanResp := scanDockerIOStackRoxImage(client, testCase.image, t)
 	scan, err := client.GetScan(context.Background(), &v1.GetScanRequest{
 		ImageSpec: scanResp.GetImage(),
 	})
@@ -100,7 +100,7 @@ func testSingleVulnImage(testCase singleTestCase, t *testing.T) {
 func TestStackroxVulnImages(t *testing.T) {
 	for _, testCase := range []singleTestCase{
 		{
-			imageTag: "django-cve-2019-14235",
+			image: "docker.io/stackrox/vuln-images:django-cve-2019-14235",
 			expectedFeatures: []feature{
 				{"django", "2.1", []expectedVuln{
 					{name: "CVE-2018-16984", fixedBy: "2.1.2"},
@@ -116,7 +116,7 @@ func TestStackroxVulnImages(t *testing.T) {
 			},
 		},
 		{
-			imageTag: "lodash-cve-2019-1010266",
+			image: "docker.io/stackrox/vuln-images:lodash-cve-2019-1010266",
 			expectedFeatures: []feature{
 				{"lodash", "4.17.10", []expectedVuln{
 					{name: "CVE-2019-10744", fixedBy: "4.17.12"},
@@ -127,7 +127,7 @@ func TestStackroxVulnImages(t *testing.T) {
 			},
 		},
 		{
-			imageTag: "rails-cve-2016-2098",
+			image: "docker.io/stackrox/vuln-images:rails-cve-2016-2098",
 			expectedFeatures: []feature{
 				{"rails", "4.2.5.1", []expectedVuln{
 					{name: "CVE-2016-2098"},
@@ -143,7 +143,7 @@ func TestStackroxVulnImages(t *testing.T) {
 		},
 		{
 			// docker.io/1and1internet/ubuntu-16-customerssh:latest
-			imageTag: "customerssh",
+			image: "docker.io/stackrox/vuln-images:customerssh",
 			expectedFeatures: []feature{
 				{"debug", "2.2.0", []expectedVuln{
 					{name: "CVE-2017-16137", fixedBy: "2.6.9"},
@@ -159,7 +159,7 @@ func TestStackroxVulnImages(t *testing.T) {
 		},
 		{
 			// appddemo/appdynamics-monitor@sha256:145ecc6c8b3a846b2c078806dbfae74b1c7dfdcde6f7931321e3763a5f898109
-			imageTag: "appdynamics",
+			image: "docker.io/stackrox/vuln-images:appdynamics",
 			expectedFeatures: []feature{
 				{"commons_beanutils", "1.9.2", []expectedVuln{
 					{name: "CVE-2019-10086", fixedBy: ""},
@@ -186,7 +186,7 @@ func TestStackroxVulnImages(t *testing.T) {
 		},
 		{
 			// docker.io/31z4/zookeeper:latest@sha256:b8b94423656f32d19a2a6ee29ceae409c82cca106ee89469c4498ceaaf3007f5
-			imageTag: "zookeeper",
+			image: "docker.io/stackrox/vuln-images:zookeeper",
 			expectedFeatures: []feature{
 				{"zookeeper", "3.4.13", []expectedVuln{
 					{name: "CVE-2019-0201", fixedBy: ""},
@@ -196,7 +196,7 @@ func TestStackroxVulnImages(t *testing.T) {
 		},
 		{
 			// docker.io/library/cassandra:latest
-			imageTag: "cassandra",
+			image: "docker.io/stackrox/vuln-images:cassandra",
 			expectedFeatures: []feature{
 				{"logback", "1.1.3", []expectedVuln{
 					{name: "CVE-2017-5929", fixedBy: ""},
@@ -212,7 +212,7 @@ func TestStackroxVulnImages(t *testing.T) {
 		},
 		{
 			// docker.io/apacheignite/ignite:latest
-			imageTag: "ignite",
+			image: "docker.io/stackrox/vuln-images:ignite",
 			expectedFeatures: []feature{
 				{"camel", "2.22.0", []expectedVuln{
 					{name: "CVE-2019-0194", fixedBy: ""},
@@ -231,7 +231,97 @@ func TestStackroxVulnImages(t *testing.T) {
 			},
 		},
 	} {
-		t.Run(testCase.imageTag, func(t *testing.T) {
+		t.Run(testCase.image, func(t *testing.T) {
+			testSingleVulnImage(testCase, t)
+		})
+	}
+}
+
+// This test checks images from gcr.io/distroless
+func TestDistrolessVulnImages(t *testing.T) {
+	for _, testCase := range []singleTestCase{
+		{
+			image: "gcr.io/distroless/base@sha256:8d58596f5181f95d908d7f8318f8e27bc394164491bd0aa53c2f284480fd8f8b",
+			expectedFeatures: []feature{
+				{"openssl", "1.1.0l-1~deb9u1", []expectedVuln{
+					{name: "CVE-2007-6755"},
+					{name: "CVE-2010-0928"},
+					{name: "CVE-2019-1551"},
+				},
+				},
+				{"glibc", "2.24-11+deb9u4", []expectedVuln{
+					{name: "CVE-2019-9192"},
+					{name: "CVE-2019-6488"},
+					{name: "CVE-2018-6551"},
+					{name: "CVE-2010-4051"},
+					{name: "CVE-2020-10029"},
+					{name: "CVE-2019-1010023"},
+					{name: "CVE-2020-1752"},
+					{name: "CVE-2020-6096"},
+					{name: "CVE-2009-5155"},
+					{name: "CVE-2010-4756"},
+					{name: "CVE-2016-10739"},
+					{name: "CVE-2019-1010025"},
+					{name: "CVE-2017-12132"},
+					{name: "CVE-2018-20796"},
+					{name: "CVE-2018-1000001"},
+					{name: "CVE-2020-1751"},
+					{name: "CVE-2019-19126"},
+					{name: "CVE-2019-7309"},
+					{name: "CVE-2019-1010024"},
+					{name: "CVE-2018-6485"},
+					{name: "CVE-2019-9169"},
+					{name: "CVE-2010-4052"},
+					{name: "CVE-2015-8985"},
+					{name: "CVE-2016-10228"},
+					{name: "CVE-2019-1010022"},
+				},
+				},
+			},
+		},
+		{
+			image: "gcr.io/distroless/java-debian10@sha256:6fa3088bb0b2df2419dda9808cdf90d706ff190cff17c2a68397ac7765b3098f",
+			expectedFeatures: []feature{
+				{"openjdk-11", "11.0.9+11-1~deb10u1", []expectedVuln{}},
+			},
+		},
+		{
+			image: "gcr.io/distroless/python2.7@sha256:6d3895c4a1629ac99e73c7dc9cbe0ad8cb213d6cdebf3e835c2c388fc5aab1b2",
+			expectedFeatures: []feature{
+				{"python2.7", "2.7.13-2+deb9u4", []expectedVuln{
+					{name: "CVE-2013-7040"},
+					{name: "CVE-2019-16935"},
+					{name: "CVE-2019-18348"},
+					{name: "CVE-2019-9674"},
+					{name: "CVE-2017-17522"},
+					{name: "CVE-2020-8492"},
+					{name: "CVE-2018-1000030"},
+				},
+				},
+			},
+		},
+		{
+			image: "gcr.io/distroless/python3@sha256:8e74b6697d0a741a5d1bb7366260f48721783f71e01d800c13cd2392586639f3",
+			expectedFeatures: []feature{
+				{"python3.5", "3.5.3-1+deb9u2", []expectedVuln{
+					{name: "CVE-2020-26116"},
+					{name: "CVE-2019-20907"},
+					{name: "CVE-2019-9674"},
+					{name: "CVE-2017-17522"},
+				},
+				},
+			},
+		},
+		{
+			image: "gcr.io/distroless/static@sha256:f4050ae771417b5a91d3906d9a9d2e7d04ccf1096d8cf88812ff15097ffde67a",
+			expectedFeatures: []feature{
+				{"netbase", "5.4", []expectedVuln{}},
+				{"tzdata", "2020a-0+deb9u1", []expectedVuln{}},
+				{"base-files", "9.9+deb9u13", []expectedVuln{}},
+			},
+		},
+	} {
+		t.Run(testCase.image, func(t *testing.T) {
 			testSingleVulnImage(testCase, t)
 		})
 	}
