5d53ad9a8dadcf6dc267ddf932d33fae240d364d
RTann <ross@stackrox.com>
2020-10-29 17:30:32 -0700

    non-bare repo

--- pkg/vulnloader/k8sloader/loader.go
+++ pkg/vulnloader/k8sloader/loader.go
@@ -28,7 +28,7 @@ func (l *loader) DownloadFeedsToPath(outputDir string) error {
 		return errors.Wrapf(err, "creating subdir for %s", vulndump.K8sDirName)
 	}
 
-	_, err := git.PlainClone(k8sDir, true, &git.CloneOptions{
+	_, err := git.PlainClone(k8sDir, false, &git.CloneOptions{
 		URL:           k8sCVEsRepository,
 		ReferenceName: "refs/heads/ross-init-cves",
 		SingleBranch:  true,
