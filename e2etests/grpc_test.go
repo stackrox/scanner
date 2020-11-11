2d3c0873d5c4d765d479f8dfe4f0fdaa522088da
RTann <ross@stackrox.com>
2020-11-10 19:26:38 -0800

    add moar

--- e2etests/grpc_test.go
+++ e2etests/grpc_test.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"testing"
 
+	"github.com/sirupsen/logrus"
 	v1 "github.com/stackrox/scanner/generated/api/v1"
 	"github.com/stackrox/scanner/pkg/types"
 	"github.com/stretchr/testify/assert"
@@ -48,11 +49,39 @@ func TestGRPCGetVulnerabilities(t *testing.T) {
 					},
 				},
 			},
+			{
+				ComponentRequest: &v1.ComponentRequest_NvdComponent{
+					NvdComponent: &v1.NVDComponentRequest{
+						Vendor:  "kubernetes",
+						Product: "cri-o",
+						Version: "1.16.0",
+					},
+				},
+			},
+			{
+				ComponentRequest: &v1.ComponentRequest_NvdComponent{
+					NvdComponent: &v1.NVDComponentRequest{
+						Vendor:  "linucfoundation",
+						Product: "containerd",
+						Version: "1.2.0",
+					},
+				},
+			},
+			{
+				ComponentRequest: &v1.ComponentRequest_NvdComponent{
+					NvdComponent: &v1.NVDComponentRequest{
+						Vendor:  "linux",
+						Product: "linux_kernel",
+						Version: "5.9.1",
+					},
+				},
+			},
 		},
 	}
 	resp, err := client.GetVulnerabilities(context.Background(), req)
 	require.NoError(t, err)
 
+	// kubelet
 	vulnList := resp.VulnerabilitiesByComponent[v1.KubernetesComponentRequest_KUBELET.String()]
 	assert.NotEmpty(t, vulnList.Vulnerabilities)
 	m := types.Metadata{
@@ -80,6 +109,7 @@ func TestGRPCGetVulnerabilities(t *testing.T) {
 	}
 	assert.Contains(t, vulnList.Vulnerabilities, cve201911245)
 
+	// docker
 	vulnList = resp.VulnerabilitiesByComponent["docker:docker:19.03.0"]
 	assert.NotEmpty(t, vulnList.Vulnerabilities)
 	m = types.Metadata{
@@ -100,10 +130,92 @@ func TestGRPCGetVulnerabilities(t *testing.T) {
 	require.NoError(t, err)
 	cve201916884 := &v1.Vulnerability{
 		Name:        "CVE-2019-16884",
-		Description: "runc through 1.0.0-rc8, as used in Docker through 19.03.2-ce and other products, allows AppArmor restriction bypass because libcontainer/rootfs_linux.go incorrectly checks mount targets, and thus a malicious Docker image can mount over a /proc directory.\n",
+		Description: "runc through 1.0.0-rc8, as used in Docker through 19.03.2-ce and other products, allows AppArmor restriction bypass because libcontainer/rootfs_linux.go incorrectly checks mount targets, and thus a malicious Docker image can mount over a /proc directory.",
 		Link:        "https://nvd.nist.gov/vuln/detail/CVE-2019-16884",
 		Metadata:    mBytes,
 		FixedBy:     "19.03.3",
 	}
+	logrus.Infof("Vulns: %+v", vulnList.Vulnerabilities)
 	assert.Contains(t, vulnList.Vulnerabilities, cve201916884)
+
+	// cri-o
+	vulnList = resp.VulnerabilitiesByComponent["kubernetes:cri-o:1.16.0"]
+	assert.NotEmpty(t, vulnList.Vulnerabilities)
+	m = types.Metadata{
+		CVSSv2: types.MetadataCVSSv2{
+			Score:               6.0,
+			Vectors:             "AV:N/AC:M/Au:S/C:P/I:P/A:P",
+			ExploitabilityScore: 6.8,
+			ImpactScore:         6.4,
+		},
+		CVSSv3: types.MetadataCVSSv3{
+			Score:               5.0,
+			Vectors:             "CVSS:3.1/AV:N/AC:H/PR:L/UI:N/S:U/C:L/I:L/A:L",
+			ExploitabilityScore: 1.6,
+			ImpactScore:         3.4,
+		},
+	}
+	mBytes, err = json.Marshal(&m)
+	require.NoError(t, err)
+	cve201914891 := &v1.Vulnerability{
+		Name:        "CVE-2019-14891",
+		Description: "A flaw was found in cri-o, as a result of all pod-related processes being placed in the same memory cgroup. This can result in container management (conmon) processes being killed if a workload process triggers an out-of-memory (OOM) condition for the cgroup. An attacker could abuse this flaw to get host network access on an cri-o host.",
+		Link:        "https://nvd.nist.gov/vuln/detail/CVE-2019-14891",
+		Metadata:    mBytes,
+		FixedBy:     "1.16.1",
+	}
+	logrus.Infof("Vulns: %+v", vulnList.Vulnerabilities)
+	assert.Contains(t, vulnList.Vulnerabilities, cve201914891)
+
+	// containerd
+	vulnList = resp.VulnerabilitiesByComponent["linuxfoundation:containerd:1.2.0"]
+	assert.NotEmpty(t, vulnList.Vulnerabilities)
+	m = types.Metadata{
+		CVSSv2: types.MetadataCVSSv2{
+			Score:               4.3,
+			Vectors:             "AV:N/AC:M/Au:N/C:P/I:N/A:N",
+			ExploitabilityScore: 8.6,
+			ImpactScore:         2.9,
+		},
+	}
+	mBytes, err = json.Marshal(&m)
+	require.NoError(t, err)
+	cve202015157 := &v1.Vulnerability{
+		Name:        "CVE-2020-15157",
+		Description: "In containerd (an industry-standard container runtime) before version 1.2.14 there is a credential leaking vulnerability. If a container image manifest in the OCI Image format or Docker Image V2 Schema 2 format includes a URL for the location of a specific image layer (otherwise known as a “foreign layer”), the default containerd resolver will follow that URL to attempt to download it. In v1.2.x but not 1.3.0 or later, the default containerd resolver will provide its authentication credentials if the server where the URL is located presents an HTTP 401 status code along with registry-specific HTTP headers. If an attacker publishes a public image with a manifest that directs one of the layers to be fetched from a web server they control and they trick a user or system into pulling the image, they can obtain the credentials used for pulling that image. In some cases, this may be the user's username and password for the registry. In other cases, this may be the credentials attached to the cloud virtual instance which can grant access to other cloud resources in the account. The default containerd resolver is used by the cri-containerd plugin (which can be used by Kubernetes), the ctr development tool, and other client programs that have explicitly linked against it. This vulnerability has been fixed in containerd 1.2.14. containerd 1.3 and later are not affected. If you are using containerd 1.3 or later, you are not affected. If you are using cri-containerd in the 1.2 series or prior, you should ensure you only pull images from trusted sources. Other container runtimes built on top of containerd but not using the default resolver (such as Docker) are not affected.",
+		Link:        "https://nvd.nist.gov/vuln/detail/CVE-2020-15157",
+		Metadata:    mBytes,
+		FixedBy:     "1.2.14",
+	}
+	logrus.Infof("Vulns: %+v", vulnList.Vulnerabilities)
+	assert.Contains(t, vulnList.Vulnerabilities, cve202015157)
+
+	// linux kernel
+	vulnList = resp.VulnerabilitiesByComponent["linux:linux_kernel:5.9.1"]
+	assert.NotEmpty(t, vulnList.Vulnerabilities)
+	m = types.Metadata{
+		CVSSv2: types.MetadataCVSSv2{
+			Score:               4.7,
+			Vectors:             "AV:L/AC:M/Au:N/C:N/I:N/A:C",
+			ExploitabilityScore: 3.4,
+			ImpactScore:         6.9,
+		},
+		CVSSv3: types.MetadataCVSSv3{
+			Score:               4.7,
+			Vectors:             "CVSS:3.1/AV:L/AC:H/PR:L/UI:N/S:U/C:N/I:N/A:H",
+			ExploitabilityScore: 1.0,
+			ImpactScore:         3.6,
+		},
+	}
+	mBytes, err = json.Marshal(&m)
+	require.NoError(t, err)
+	cve202027675 := &v1.Vulnerability{
+		Name:        "CVE-2020-27675",
+		Description: "An issue was discovered in the Linux kernel through 5.9.1, as used with Xen through 4.14.x. drivers/xen/events/events_base.c allows event-channel removal during the event-handling loop (a race condition). This can cause a use-after-free or NULL pointer dereference, as demonstrated by a dom0 crash via events for an in-reconfiguration paravirtualized device, aka CID-073d0552ead5.",
+		Link:        "https://nvd.nist.gov/vuln/detail/CVE-2020-27675",
+		Metadata:    mBytes,
+		FixedBy:     "",
+	}
+	logrus.Infof("Vulns: %+v", vulnList.Vulnerabilities)
+	assert.Contains(t, vulnList.Vulnerabilities, cve202027675)
 }
