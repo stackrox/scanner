98d91c2492e50aae4d8786a6819ec5df7e8ae1ed
Connor Gorman <connorgorman@users.noreply.github.com>
2020-12-04 15:13:26 -0800

    Only attribute vulns to the core runtime because the version is nuget (#257)

--- cpe/attributes/dotnetcoreruntime/dotnetcoreruntime.go
+++ cpe/attributes/dotnetcoreruntime/dotnetcoreruntime.go
@@ -1,7 +1,6 @@
 package dotnetcoreruntime
 
 import (
-	"path/filepath"
 	"strings"
 
 	"github.com/facebookincubator/nvdtools/wfn"
@@ -14,18 +13,7 @@ func GetDotNetCoreRuntimeAttributes(c *component.Component) []*wfn.Attributes {
 	vendorSet := set.NewStringSet("microsoft")
 
 	nameSet := set.NewStringSet(c.Name, escapePeriod(c.Name))
-	versionSet := set.NewStringSet()
-	if filepath.Ext(c.Location) == ".dll" {
-		// If the file is a DLL, then the version strings are of the format 4.0.0.0
-		// but the vulnerabilities only describe in the style of 4.0.0
-		version := c.Version
-		if lastIdx := strings.LastIndex(c.Version, "."); lastIdx != -1 {
-			version = version[:lastIdx]
-		}
-		versionSet.AddAll(version, escapePeriod(version))
-	} else {
-		versionSet.AddAll(c.Version, escapePeriod(c.Version))
-	}
+	versionSet := set.NewStringSet(c.Version, escapePeriod(c.Version))
 	return common.GenerateAttributesFromSets(vendorSet, nameSet, versionSet, "")
 }
 
