bffa97d8b0690d93107b2b160f76cc442517bcee
Mandar Darwatkar <mandar@stackrox.com>
2020-12-04 19:07:32 -0800

    [ROX-6080] Add endpoint for vulns defs metadata (#260)
    * Add endpoint for vulns defs metadata

--- e2etests/grpc_test.go
+++ e2etests/grpc_test.go
@@ -228,3 +228,11 @@ func TestGRPCGetVulnerabilities(t *testing.T) {
 	}
 	assert.Contains(t, vuln.Vulnerabilities, cve202027675)
 }
+
+func TestGRPCVulnDefsMetadata(t *testing.T) {
+	conn := connectToScanner(t)
+	client := v1.NewVulnDefsServiceClient(conn)
+	metadata, err := client.GetVulnDefsMetadata(context.Background(), &v1.Empty{})
+	require.NoError(t, err)
+	assert.NotNil(t, metadata.GetLastUpdatedTime())
+}
