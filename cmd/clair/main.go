bf51fe1c551e22509072de44af49471916072096
md2119 <mandar@stackrox.com>
2020-12-08 11:03:46 -0800

    rebase

--- cmd/clair/main.go
+++ cmd/clair/main.go
@@ -150,12 +150,12 @@ func Boot(config *Config) {
 		grpcAPI := grpc.NewAPI(grpc.Config{
 			Port:         port,
 			CustomRoutes: debugRoutes,
-		})
+		}, manager)
 
 		grpcAPI.Register(
 			ping.NewService(),
-			scan.NewService(manager, db, nvdVulnCache, k8sVulnCache),
-			vulndefs.NewService(manager, db),
+			scan.NewService(db, nvdVulnCache, k8sVulnCache),
+			vulndefs.NewService(db),
 		)
 
 		go grpcAPI.Start()
