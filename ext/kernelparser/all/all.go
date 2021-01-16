ac1e9a5096c770da92dd574831bd788ee2c914c6
RTann <ross@stackrox.com>
2021-01-15 16:52:26 -0800

    style

--- ext/kernelparser/all/all.go
+++ ext/kernelparser/all/all.go
@@ -3,8 +3,8 @@ package all
 import (
 	// Import all the kernelparsers
 	_ "github.com/stackrox/scanner/ext/kernelparser/amzn"
+	_ "github.com/stackrox/scanner/ext/kernelparser/cos"
 	_ "github.com/stackrox/scanner/ext/kernelparser/debian"
 	_ "github.com/stackrox/scanner/ext/kernelparser/rhel"
 	_ "github.com/stackrox/scanner/ext/kernelparser/ubuntu"
-	_ "github.com/stackrox/scanner/ext/kernelparser/cos"
 )
