0b481b986e76e770f1ef7e9078df5b6360114cec
Ross <ross@stackrox.com>
2021-02-04 00:22:11 -0800

    Update OS support early 2021 (#298)

--- pkg/wellknownnamespaces/set.go
+++ pkg/wellknownnamespaces/set.go
@@ -3,6 +3,7 @@ package wellknownnamespaces
 import "github.com/stackrox/rox/pkg/set"
 
 var (
+	// KnownStaleNamespaces is the set of base namespaces we know have stale vulnerabilities.
 	KnownStaleNamespaces = set.NewFrozenStringSet(
 		"alpine:v3.2",
 		"alpine:v3.3",
@@ -11,6 +12,7 @@ var (
 		"alpine:v3.6",
 		"alpine:v3.7",
 		"debian:8",
+		"oracle:5",
 		"ubuntu:12.10",
 		"ubuntu:13.04",
 		"ubuntu:14.10",
@@ -24,12 +26,16 @@ var (
 		"ubuntu:19.10",
 	)
 
+	// KnownSupportedNamespaces is the set of base namespaces we support.
+	// If you add a new Debian or Ubuntu version, be sure to also add it to
+	// database/namespace_mapping.go.pkg/wellknownnamespaces/set.go
 	KnownSupportedNamespaces = set.NewFrozenStringSet(
 		"alpine:v3.8",
 		"alpine:v3.9",
 		"alpine:v3.10",
 		"alpine:v3.11",
 		"alpine:v3.12",
+		"alpine:v3.13",
 		"amzn:2018.03",
 		"amzn:2",
 		"centos:5", // TODO: EOL 11/2020
@@ -40,7 +46,6 @@ var (
 		"debian:10",
 		"debian:11",
 		"debian:unstable",
-		"oracle:5", // TODO: EOL 11/2020
 		"oracle:6",
 		"oracle:7",
 		"oracle:8",
