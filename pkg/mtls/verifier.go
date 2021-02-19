805ca31bbcbda99ac163685c570019e69e5efcfd
Connor Gorman <cgorman@stackrox.com>
2021-02-19 11:17:33 -0800

    Check OU instead of CN due to old certificate logic

--- pkg/mtls/verifier.go
+++ pkg/mtls/verifier.go
@@ -2,15 +2,16 @@ package mtls
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/pkg/errors"
 )
 
 const (
-	centralCN = "CENTRAL_SERVICE: Central"
+	centralOU = "CENTRAL_SERVICE"
 )
 
-// VerifyCentralPeerCertificate verifies that the peer certificate has the Central Common Name
+// VerifyCentralPeerCertificate verifies that the peer certificate has the Central Organizational Unit
 // The CA should have already been verified via tls.VerifyClientCertIfGiven
 func VerifyCentralPeerCertificate(r *http.Request) error {
 	if r.TLS == nil {
@@ -20,8 +21,15 @@ func VerifyCentralPeerCertificate(r *http.Request) error {
 	if len(peerCerts) == 0 {
 		return errors.New("no peer certificates found")
 	}
-	if peerCN := peerCerts[0].Subject.CommonName; peerCN != centralCN {
-		return errors.Errorf("peer certificate common name %q does not match expected common name: %s", peerCN, centralCN)
+	organizationalUnits := peerCerts[0].Subject.OrganizationalUnit
+	if len(organizationalUnits) == 0 {
+		return errors.New("peer certificate does not contain an OU")
 	}
-	return nil
+
+	for _, ou := range organizationalUnits {
+		if strings.EqualFold(ou, centralOU) {
+			return nil
+		}
+	}
+	return errors.Errorf("peer certificate OUs %+v does not match expected common name: %s", organizationalUnits, centralOU)
 }
