ca321c7ae479440b6ba46c06ccd4feddd3c21c8f
RTann <ross@stackrox.com>
2021-03-10 14:53:49 -0800

    logs pls

--- ext/vulnmdsrc/nvd/nvd.go
+++ ext/vulnmdsrc/nvd/nvd.go
@@ -69,8 +69,11 @@ func (a *appender) BuildCache(dumpDir string) error {
 
 	fileInfos, err := ioutil.ReadDir(dumpDir)
 	if err != nil {
+		log.Error("NVD fail")
 		return errors.Wrap(err, "failed to read dir")
 	}
+
+	log.Info("NVD build cache continuing...")
 
 	for _, fileInfo := range fileInfos {
 		fileName := fileInfo.Name()
