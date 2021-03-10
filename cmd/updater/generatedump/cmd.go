9b5b84d5160095e0c26c82659f962e6f9a69bc71
RTann <ross@stackrox.com>
2021-03-10 13:55:20 -0800

    logs

--- cmd/updater/generatedump/cmd.go
+++ cmd/updater/generatedump/cmd.go
@@ -163,6 +163,8 @@ func addMetadata(vulnerabilities []database.Vulnerability, dumpDir string) ([]da
 		}
 	}
 
+	log.Info("Hi")
+
 	for i := range vulnerabilities {
 		vuln := &vulnerabilities[i]
 		appender := vulnmdsrc.AppenderForVuln(vuln)
@@ -170,6 +172,8 @@ func addMetadata(vulnerabilities []database.Vulnerability, dumpDir string) ([]da
 			return nil, errors.Wrapf(err, "Failed to append metadata for vuln %s", vuln.Name)
 		}
 	}
+
+	log.Info("Bye")
 
 	return vulnerabilities, nil
 }
