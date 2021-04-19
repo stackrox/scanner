6fdb7c0eb3dd92ed7c0934cc491bcec27b540472
RTann <rtann@redhat.com>
2021-04-19 14:14:52 -0700

    add note

--- api/v1/models.go
+++ api/v1/models.go
@@ -274,8 +274,12 @@ func LayerFromDatabaseModel(db database.Datastore, dbLayer database.Layer, withF
 			layer.Features = append(layer.Features, *feature)
 		}
 		if strings.HasPrefix(layer.NamespaceName, "rhel") {
-			if err := addRHELv2Vulns(db, &layer); err != nil {
+			certified, err := addRHELv2Vulns(db, &layer)
+			if err != nil {
 				return layer, notes, err
+			}
+			if !certified {
+				notes = append(notes, CertifiedRHELScanUnavailable)
 			}
 		}
 		if env.LanguageVulns.Enabled() {
@@ -313,19 +317,25 @@ func updateFeatureWithVulns(feature *Feature, dbVulns []database.Vulnerability, 
 // https://github.com/quay/claircore
 ///////////////////////////////////////////////////
 
-func addRHELv2Vulns(db database.Datastore, layer *Layer) error {
+
+// addRHELv2Vulns appends vulnerabilities found during RHELv2 scanning.
+// RHELv2 scanning performs the scanning/analysis needed to be
+// certified as part of Red Hat's Scanner Certification Program.
+// The returned bool indicates if full certified scanning was performed.
+// This is typically only `false` for images without proper CPE information.
+func addRHELv2Vulns(db database.Datastore, layer *Layer) (bool, error) {
 	layers, err := db.GetRHELv2Layers(layer.Name)
 	if err != nil {
-		return err
+		return false, err
 	}
 
-	shareCPEs(layers)
+	cpesExist := shareCPEs(layers)
 
 	pkgEnvs, records := getRHELv2PkgData(layers)
 
 	vulns, err := db.GetRHELv2Vulnerabilities(records)
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	for _, pkgEnv := range pkgEnvs {
@@ -378,22 +388,27 @@ func addRHELv2Vulns(db database.Datastore, layer *Layer) error {
 		layer.Features = append(layer.Features, feature)
 	}
 
-	return nil
+	return cpesExist, nil
 }
 
 func rpmVersionPtr(ver rpmVersion.Version) *rpmVersion.Version {
 	return &ver
 }
 
-// shareRepos takes repository definition and share it with other layers
-// where repositories are missing
-func shareCPEs(layers []*database.RHELv2Layer) {
+// shareRepos takes repository CPEs and shares it with other layers
+// where CPEs are missing.
+// Return a bool indicating if any CPEs exist.
+func shareCPEs(layers []*database.RHELv2Layer) bool {
+	var cpesExist bool
+
 	// User's layers build on top of Red Hat images doesn't have a repository definition.
 	// We need to share CPE repo definition to all layer where CPEs are missing
 	var previousCPEs []string
 	for i := 0; i < len(layers); i++ {
 		if len(layers[i].CPEs) != 0 {
 			previousCPEs = layers[i].CPEs
+
+			cpesExist = true
 		} else {
 			layers[i].CPEs = append(layers[i].CPEs, previousCPEs...)
 		}
@@ -410,6 +425,8 @@ func shareCPEs(layers []*database.RHELv2Layer) {
 			layers[i].CPEs = append(layers[i].CPEs, previousCPEs...)
 		}
 	}
+
+	return cpesExist
 }
 
 func getRHELv2PkgData(layers []*database.RHELv2Layer) (map[int]*database.RHELv2PackageEnv, []*database.RHELv2Record) {
@@ -654,6 +671,10 @@ const (
 	// LanguageCVEsUnavailable labels scans of images with without language CVEs.
 	// This is typically only populated when language CVEs are not enabled.
 	LanguageCVEsUnavailable
+	// CertifiedRHELScanUnavailable labels scans of RHEL-based images out-of-scope
+	// of the Red Hat Certification program.
+	// These images were made before June 2020, and they are missing content manifest JSON files.
+	CertifiedRHELScanUnavailable
 )
 
 type VulnerabilityEnvelope struct {
