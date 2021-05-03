7f938b6ad5cbe70395eebbedf704cc666f619930
RTann <rtann@redhat.com>
2021-05-02 23:48:43 -0700

    style

--- e2etests/components_test.go
+++ e2etests/components_test.go
@@ -94,7 +94,7 @@ func TestRemovedComponents(t *testing.T) {
 		t.Run(c.distro, func(t *testing.T) {
 			scanResp := scanDockerIOStackRoxImage(client, fmt.Sprintf("stackrox/vuln-images:%s-package-removal", c.distro), true, t)
 			scan, err := client.GetImageScan(context.Background(), &v1.GetImageScanRequest{
-				ImageSpec: scanResp.GetImage(),
+				ImageSpec:       scanResp.GetImage(),
 				UncertifiedRHEL: true,
 			})
 			require.NoError(t, err)
