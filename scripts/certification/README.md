11811872cb4136d86fa29841067ba2f27d784368
RTann <rtann@redhat.com>
2021-05-31 13:12:08 -0700

    add certification scripts

---
+++ scripts/certification/README.md
@@ -0,0 +1,23 @@
+# Red Hat Scanner Certification
+
+Upon major changes to our RHEL image scanning functionality
+or updates to the certification program, we must resubmit results
+to Red Hat.
+
+We only need to follow steps 3 - 8 from the
+[certification workflow](https://redhat-connect.gitbook.io/partner-guide-red-hat-vulnerability-scanner-cert/workflow).
+
+## How to Use
+
+1. Start rox locally
+1. Run `./run.sh`
+1. Send the output CSV files to Red Hat
+
+## TODO
+
+* Support non-local rox endpoint
+
+## References
+
+* [Red Hat Scanner Certification](https://redhat-connect.gitbook.io/partner-guide-red-hat-vulnerability-scanner-cert/)
+* [OVAL v2](https://redhat-connect.gitbook.io/partner-guide-for-adopting-red-hat-oval-v2/)
---
+++ scripts/certification/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function cli() {
+  if [[ -z $ROX_API_TOKEN ]]; then
+    roxctl --insecure-skip-tls-verify -e localhost:8000 -p $(gpp) $@
+  else
+    roxctl --insecure-skip-tls-verify -e localhost:8000 $@
+  fi
+}
+
+function jq_csv() {
+  jq -r '.scan.components[]? | { name: .name, version: .version, vulns: .vulns[]? | { cve: .cve, cvss: .cvss, fixedBy: .fixedBy, link: .link, severity: .severity } | flatten } | flatten | @csv'
+}
+
+images=("stackrox/sandbox:nodejs-10" "stackrox/sandbox:jenkins-agent-maven-35-rhel7")
+names=("nodejs" "jenkins")
+
+i=0
+for img in ${images[@]}; do
+  name=${names[i]}
+  cli image scan -i $img | jq_csv > output.csv
+  python to_csv.py output.csv "$name".csv
+  rm output.csv
+  i=$((i+1))
+done
+
+
+
