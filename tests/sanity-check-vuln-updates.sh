2e26723eeb06aa3c05626074c295c27af3a036d3
Shane Bostick <sbostick@redhat.com>
2021-07-08 15:58:05 -0700

    improve step-wise verifiability and error reporting in sanity-check-vuln-updates.sh test

--- tests/sanity-check-vuln-updates.sh
+++ tests/sanity-check-vuln-updates.sh
@@ -16,6 +16,22 @@ set -eu
 function is_mac   { uname | grep -qi 'darwin'; }
 function is_linux { uname | grep -qi 'linux'; }
 
+function manual_repro_check {
+  local diff_id=${1:-e8f8b9ab-6a75-433a-8b86-15586ec41a7b}
+
+  cf_url="https://definitions.stackrox.io/$diff_id/diff.zip"
+  gcs_https_url="https://storage.googleapis.com/definitions.stackrox.io/$diff_id/diff.zip"
+  gcs_gs_url="gs://definitions.stackrox.io/$diff_id/diff.zip"
+
+  echo "diff_id => $diff_id"
+  echo "cf_url => $cf_url"
+  wget -q "$cf_url" && unzip -q -c diff.zip manifest.json | jq -cr '.' && rm -f diff.zip
+  echo "gcs_https_url => $gcs_https_url"
+  wget -q "$gcs_https_url" && unzip -q -c diff.zip manifest.json | jq -cr '.' && rm -f diff.zip
+  echo "gcs_gs_url=> $gcs_gs_url"
+  gsutil stat "$gcs_gs_url" | sed -Ene 's/^ *(Update time:.*)/\1/'
+}
+
 function parse_date_to_epoch_sec {
   local date_string date_format
   date_string=$1
@@ -29,19 +45,53 @@ function parse_date_to_epoch_sec {
 }
 
 function get_manifest_content_from_zip {
-  local fpath_zipfile
-  fpath_zipfile=$1
-  unzip -q -c "$fpath_zipfile" manifest.json
+  local fpath_zipfile=$1
+  unzip -q -c "$fpath_zipfile" manifest.json || error "failed to unzip '$fpath_zipfile'"
+}
+
+function validate_manifest_until {
+  if ! echo "$1" | grep -qE "^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2} GMT$"; then
+    bash_exit_failure "BAD manifest_until value [$1]"
+  fi
+}
+
+function validate_epoch_sec {
+  if ! echo "$1" | grep -qE "^\d{10}$"; then
+    bash_exit_failure "BAD epoch_sec value [$1]"
+  fi
+}
+
+function validate_manifest_content {
+  if ! echo "$1" | jq -r '.' &>/dev/null; then
+    bash_exit_failure "BAD manifest_content [$1]"
+  fi
+}
+
+function validate_integer {
+  if ! echo "$1" | grep -qE "^\d+$"; then
+    bash_exit_failure "BAD integer [$1]"
+  fi
 }
 
 function get_manifest_age_seconds_from_zip {
-  local fpath_zipfile manifest_until_raw manifest_until_epoch_sec now_epoch_sec age_seconds
+  local fpath_zipfile manifest_raw manifest_until_raw manifest_until_epoch_sec now_epoch_sec age_seconds
   fpath_zipfile=$1
-  manifest_until_raw=$(get_manifest_content_from_zip "$fpath_zipfile" \
-      | jq -r ".until" | sed -Ee 's#\.[0-9]+Z# GMT#')
-  manifest_until_epoch_sec=$(parse_date_to_epoch_sec "$manifest_until_raw" "%Y-%M-%dT%H:%M:%S %Z")
+
+  manifest_content=$(get_manifest_content_from_zip "$fpath_zipfile")
+  validate_manifest_content "$manifest_content"
+
+  manifest_until=$(echo "$manifest_content" | jq -r ".until" | sed -Ee 's#\.[0-9]+Z# GMT#')
+  validate_manifest_until "$manifest_until"
+
+  manifest_until_epoch_sec=$(parse_date_to_epoch_sec "$manifest_until" "%Y-%M-%dT%H:%M:%S %Z")
+  validate_epoch_sec "$manifest_until_epoch_sec"
+
   now_epoch_sec=$(date "+%s")
+  validate_epoch_sec "$now_epoch_sec"
+
   age_seconds=$(( now_epoch_sec - manifest_until_epoch_sec ))
+  validate_integer "$age_seconds"
+
   echo "$age_seconds"
 }
 
@@ -65,10 +115,12 @@ function run_tests_for_diff_id {
   info "downloading diff1.zip from $DIFF1_CLOUDFLARE_URL"
   diff1_cache_control=$(curl -s -o ./diff1.zip -v "$DIFF1_CLOUDFLARE_URL" 2>&1 \
     | grep "cache-control" | sed -e "s#^< ##g; s#\r##g;")
+  [[ $? -eq 0 ]] || bash_exit_failure "curl failed on $DIFF1_CLOUDFLARE_URL"
 
   info "downloading diff2.zip from $DIFF2_GCS_URL"
   diff2_cache_control=$(curl -s -o ./diff2.zip -v "$DIFF2_GCS_URL" 2>&1 \
     | grep "cache-control" | sed -e "s#^< ##g; s#\r##g;")
+  [[ $? -eq 0 ]] || bash_exit_failure "curl failed on $DIFF2_GCS_URL"
 
   local gcs_object_age_seconds diff1_manifest_content diff2_manifest_content \
     diff1_manifest_age_seconds diff2_manifest_age_seconds diff1_archive_md5 \
@@ -117,20 +169,19 @@ function run_tests_for_diff_id {
 }
 
 function get_gcs_object_age_seconds {
-  local diff_id created_datestamp_raw created_datestamp_epoch_sec \
-    now_epoch_sec obj_age_seconds
+  local diff_id created_datestamp_raw created_datestamp_epoch_sec
+  local now_epoch_sec obj_age_seconds
 
   diff_id="$1"
-  created_datestamp_raw=$(grep -A2 "$diff_id" "$FPATH_DIFF_GSUTIL_STAT" \
-    | grep "Creation time:" | sed -Ee 's/ +/ /g; s/^ +//;' | cut -d' ' -f3-)
-  created_datestamp_epoch_sec=$(parse_date_to_epoch_sec "$created_datestamp_raw" "%a, %d %b %Y %H:%M:%S %Z")
+  created_time_raw=$(grep -A2 "$diff_id" "$FPATH_DIFF_GSUTIL_STAT" | sed -Ene 's/^ +Creation time: +(.*)/\1/p')
+  created_time_epoch_sec=$(parse_date_to_epoch_sec "$created_time_raw" "%a, %d %b %Y %H:%M:%S %Z")
   now_epoch_sec=$(date "+%s")
-  obj_age_seconds=$(( now_epoch_sec - created_datestamp_epoch_sec ))
+  obj_age_seconds=$(( now_epoch_sec - created_time_epoch_sec ))
   echo "$obj_age_seconds"
 }
 
-function info { >&1 echo "INFO: $*"; }
-function warn { >&1 echo "WARN: $*"; }
+function info { >&2 echo "INFO: $*"; }
+function warn { >&2 echo "WARN: $*"; }
 function error { >&2 echo "ERROR: $*"; }
 function testfail { >&2 echo "FAIL: $*"; (( FAILURE_COUNT += 1 )); }
 function bash_true { ((0 == 0)); }
@@ -163,24 +214,21 @@ else
   gsutil ls -r "gs://definitions.stackrox.io/*/diff.zip" > "$FPATH_DIFF_LIST"
 fi
 
-# Extract the ids (uniquely identifying hashes).
-sed -Ee "s#gs://definitions.stackrox.io/##g; s#/diff.zip##g;" \
-  < "$FPATH_DIFF_LIST" > "$FPATH_DIFF_ID_LIST"
+# Extract the ids (uniquely identifying hashes)
+sed -Ee "s#gs://definitions.stackrox.io/##g; s#/diff.zip##g;" < "$FPATH_DIFF_LIST" > "$FPATH_DIFF_ID_LIST"
 
 # List metadata for each diff
-gsutil stat "$(cat $FPATH_DIFF_LIST)" > "$FPATH_DIFF_GSUTIL_STAT"
+gsutil stat $(cat "$FPATH_DIFF_LIST") > "$FPATH_DIFF_GSUTIL_STAT"
 
 # Check metadata for each diffs
 while read -r line; do
   run_tests_for_diff_id "$line"
 done < "$FPATH_DIFF_ID_LIST"
 
-# Cleanup files we don't want archived by the ci job
+# Cleanup, report, exit
 rm -f diff{1,2}.zip
-
 info "--------"
 info "ran to completion -- see $FPATH_TRANSCRIPT"
-
 if [[ $FAILURE_COUNT -gt 0 ]]; then
   bash_exit_failure "$FAILURE_COUNT test failures"
 fi
