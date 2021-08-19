2b51da2e39097b00ba181581b086639e359eb457
Cong Du <c-du@users.noreply.github.com>
2021-08-19 14:31:22 -0700

    feature on

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -5,5 +5,5 @@ var (
 	ContinueUnknownOS = registerFeature("Enable continuation upon detecting unknown OS", "ROX_CONTINUE_UNKNOWN_OS", true)
 
 	// ActiveVulnMgmt defines if features related to Active Vuln Mgmt should be enabled.
-	ActiveVulnMgmt = registerFeature("Enable features related to Active Vulnerability Management", "ROX_ACTIVE_VULN_MGMT", false)
+	ActiveVulnMgmt = registerFeature("Enable features related to Active Vulnerability Management", "ROX_ACTIVE_VULN_MGMT", true)
 )
