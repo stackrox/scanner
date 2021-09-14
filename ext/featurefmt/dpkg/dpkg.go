897b213b9db1880de8a8ec693467993aa3236d67
RTann <rtann@redhat.com>
2021-09-13 17:17:33 -0700

    fix

--- ext/featurefmt/dpkg/dpkg.go
+++ ext/featurefmt/dpkg/dpkg.go
@@ -18,15 +18,17 @@ package dpkg
 import (
 	"bufio"
 	"bytes"
-	"net/mail"
 	"regexp"
 	"sort"
 	"strings"
 	"time"
 
+	"github.com/stackrox/rox/pkg/stringutils"
+
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/stackrox/rox/pkg/set"
+	"github.com/stackrox/rox/pkg/utils"
 	"github.com/stackrox/scanner/database"
 	"github.com/stackrox/scanner/ext/featurefmt"
 	"github.com/stackrox/scanner/ext/versionfmt"
@@ -59,11 +61,13 @@ func init() {
 	featurefmt.RegisterLister("dpkg", &lister{})
 }
 
-///////////////////////////////////////////////////
-// BEGIN
-// Influenced by ClairCore under Apache 2.0 License
-// https://github.com/quay/claircore
-///////////////////////////////////////////////////
+type componentMetadata struct {
+	name          string
+	version       string
+	sourceName    string
+	sourceVersion string
+	arch          string
+}
 
 func (l lister) parseComponents(files tarutil.FilesMap, file []byte, packagesMap map[featurefmt.PackageKey]*database.FeatureVersion, removedPackages set.StringSet, distroless bool) error {
 	pkgFmt := `dpkg`
@@ -72,146 +76,141 @@ func (l lister) parseComponents(files tarutil.FilesMap, file []byte, packagesMap
 	}
 	defer metrics.ObserveListFeaturesTime(pkgFmt, "all", time.Now())
 
-	// The database is actually an RFC822-like message with "\n\n"
-	// separators, so don't be alarmed by the usage of the "net/mail"
-	// package here.
 	scanner := bufio.NewScanner(bytes.NewReader(file))
-	scanner.Split(dbSplitFunc)
+
+	var pkgMetadata componentMetadata
 	for scanner.Scan() {
-		msg, err := mail.ReadMessage(bytes.NewReader(scanner.Bytes()))
-		if err != nil {
-			log.WithError(err).Warning("could not parse dpkg db entry. skipping")
+		line := scanner.Text()
+		switch {
+		case line == "": // Delimits end of package definition. Go to the bottom of the loop.
+		case strings.HasPrefix(line, "Package: "):
+			pkgMetadata.name = line[len("Package: "):]
 			continue
-		}
-
-		currPkgName := msg.Header.Get("Package")
-
-		if strings.Contains(msg.Header.Get("Status"), "deinstall") {
-			// This package is meant to be uninstalled, so ignore it.
-			removedPackages.Add(currPkgName)
+		case strings.HasPrefix(line, "Version: "):
+			version := line[len("Version: "):]
+			err := versionfmt.Valid(dpkg.ParserName, version)
+			if err != nil {
+				log.WithError(err).WithFields(map[string]interface{}{"name": pkgMetadata.name, "version": pkgMetadata.version}).Warning("could not parse package version. skipping")
+				continue
+			}
+			pkgMetadata.version = version
 			continue
-		}
-
-		currPkgVersion := msg.Header.Get("Version")
-		err = versionfmt.Valid(dpkg.ParserName, currPkgVersion)
-		if err != nil {
-			log.WithError(err).WithFields(map[string]interface{}{"name": currPkgName, "version": currPkgVersion}).Warning("could not parse package version. skipping")
+		case strings.HasPrefix(line, "Architecture: "):
+			pkgMetadata.arch = line[len("Architecture: "):]
 			continue
-		}
-
-		var sourceName, sourceVersion string
-
-		// Get the package's source, if it exists.
-		if src := msg.Header.Get("Source"); src != "" {
+		case strings.HasPrefix(line, "Status: "):
+			if strings.Contains(line, "deinstall") {
+				// It is assumed the package's name has already been determined at this point.
+				removedPackages.Add(pkgMetadata.name)
+			}
+			continue
+		case strings.HasPrefix(line, "Source: "):
 			md := make(map[string]string)
 
-			srcCapture := dpkgSrcCaptureRegexp.FindAllStringSubmatch(src, -1)[0]
+			srcCapture := dpkgSrcCaptureRegexp.FindAllStringSubmatch(line[len("Source: "):], -1)[0]
 			for i, n := range srcCapture {
 				md[dpkgSrcCaptureRegexpNames[i]] = strings.TrimSpace(n)
 			}
 
-			sourceName = md["name"]
-			sourceVersion = currPkgVersion
+			pkgMetadata.sourceName = md["name"]
+			pkgMetadata.sourceVersion = pkgMetadata.version
 
 			if md["version"] != "" {
 				version := md["version"]
-				err = versionfmt.Valid(dpkg.ParserName, version)
+				err := versionfmt.Valid(dpkg.ParserName, version)
 				if err != nil {
-					log.WithError(err).WithFields(log.Fields{"name": sourceName, "version": version}).Warning("could not parse source package version. skipping")
+					log.WithError(err).WithFields(log.Fields{"name": pkgMetadata.sourceName, "version": version}).Warning("could not parse source package version. skipping")
 					continue
 				}
 
-				sourceVersion = version
-			}
-		}
-
-		// Debian and Ubuntu vulnerability feeds only have entries for source packages,
-		// and not the package binaries, though usually only the binaries are installed.
-		pkgName := sourceName
-		pkgVersion := sourceVersion
-		if pkgName == "" {
-			pkgName = currPkgName
-			pkgVersion = currPkgVersion
-		}
-
-		var executables []string
-		// Distroless containers do not provide executable files the same way distro containers do.
-		if !distroless && features.ActiveVulnMgmt.Enabled() {
-			// for example: var/lib/dpkg/info/vim.list
-			filenamesList := dpkgInfoPrefix + currPkgName + dpkgFilenamesSuffix
-			arch := msg.Header.Get("Architecture")
-			// for example: /var/lib/dpkg/info/zlib1g:amd64.list
-			filenamesArchList := dpkgInfoPrefix + currPkgName + ":" + arch + dpkgFilenamesSuffix
-
-			// Read the list of files provided by the current package.
-			filenamesFileData := files[filenamesList]
-			if len(filenamesFileData.Contents) == 0 {
-				filenamesFileData = files[filenamesArchList]
+				pkgMetadata.sourceVersion = version
 			}
 
-			filenamesFileScanner := bufio.NewScanner(bytes.NewReader(filenamesFileData.Contents))
-			for filenamesFileScanner.Scan() {
-				filename := filenamesFileScanner.Text()
-
-				// The first character is always "/", which is removed when inserted into the files maps.
-				if fileData := files[filename[1:]]; fileData.Executable {
-					executables = append(executables, filename)
-				}
-			}
-			if err := filenamesFileScanner.Err(); err != nil {
-				log.WithError(err).WithFields(log.Fields{"name": currPkgName, "version": currPkgVersion}).Warning("could not parse provided file list")
-				// Even though there is an error, do not skip the package.
-				// The active vulnerability management analysis may be incomplete, but that should not prevent us from
-				// performing typical vulnerability management analysis.
-			}
-		}
-
-		key := featurefmt.PackageKey{
-			Name:    pkgName,
-			Version: pkgVersion,
-		}
-
-		// If the package already exists, then this must be a source package
-		// with multiple associated packages.
-		if feature, exists := packagesMap[key]; exists {
-			// Append the executable files for the associated package to the source package.
-			feature.ProvidedExecutables = append(feature.ProvidedExecutables, executables...)
+			continue
+		default:
+			// We do not care about the other fields at this time.
 			continue
 		}
 
-		packagesMap[key] = &database.FeatureVersion{
-			Feature: database.Feature{
-				Name: pkgName,
-			},
-			Version:             pkgVersion,
-			ProvidedExecutables: executables,
-		}
+		// We must only be here if we are on an empty line, which delimits the end of a package.
+		utils.Must(func() error {
+			if line == "" {
+				return nil
+			}
+			return errors.New("must not be here unless there is a programming error")
+		}())
+
+		handleComponent(files, &pkgMetadata, packagesMap, removedPackages, distroless)
+
+		pkgMetadata = componentMetadata{}
 	}
 
 	return scanner.Err()
 }
 
-// dbSplitFunc is a bufio.SplitFunc that looks for a double-newline and leaves it
-// attached to the resulting token.
-func dbSplitFunc(data []byte, atEOF bool) (int, []byte, error) {
-	const delim = "\n\n"
-	if atEOF && len(data) == 0 {
-		return 0, nil, nil
-	}
-	if i := bytes.Index(data, []byte(delim)); i >= 0 {
-		return i + len(delim), data[:i+len(delim)], nil
-	}
-	if atEOF {
-		return len(data), data, nil
-	}
-	return 0, nil, nil
-}
+func handleComponent(files tarutil.FilesMap, pkgMetadata *componentMetadata, packagesMap map[featurefmt.PackageKey]*database.FeatureVersion, removedPackages set.StringSet, distroless bool) {
+	// Debian and Ubuntu vulnerability feeds only have entries for source packages,
+	// and not the package binaries, though usually only the binaries are installed.
+	pkgName := stringutils.FirstNonEmpty(pkgMetadata.sourceName, pkgMetadata.name)
+	pkgVersion := stringutils.FirstNonEmpty(pkgMetadata.sourceVersion, pkgMetadata.version)
 
-///////////////////////////////////////////////////
-// END
-// Influenced by ClairCore under Apache 2.0 License
-// https://github.com/quay/claircore
-///////////////////////////////////////////////////
+	// Sanity check the package definition.
+	if pkgName == "" || pkgVersion == "" || removedPackages.Contains(pkgMetadata.name) {
+		return
+	}
+
+	var executables []string
+	// Distroless containers do not provide executable files the same way distro containers do.
+	if !distroless && features.ActiveVulnMgmt.Enabled() {
+		// for example: var/lib/dpkg/info/vim.list
+		filenamesList := dpkgInfoPrefix + pkgMetadata.name + dpkgFilenamesSuffix
+		// for example: /var/lib/dpkg/info/zlib1g:amd64.list
+		filenamesArchList := dpkgInfoPrefix + pkgMetadata.name + ":" + pkgMetadata.arch + dpkgFilenamesSuffix
+
+		// Read the list of files provided by the current package.
+		filenamesFileData := files[filenamesList]
+		if len(filenamesFileData.Contents) == 0 {
+			filenamesFileData = files[filenamesArchList]
+		}
+
+		filenamesFileScanner := bufio.NewScanner(bytes.NewReader(filenamesFileData.Contents))
+		for filenamesFileScanner.Scan() {
+			filename := filenamesFileScanner.Text()
+
+			// The first character is always "/", which is removed when inserted into the files maps.
+			if fileData := files[filename[1:]]; fileData.Executable {
+				executables = append(executables, filename)
+			}
+		}
+		if err := filenamesFileScanner.Err(); err != nil {
+			log.WithError(err).WithFields(log.Fields{"name": pkgMetadata.name, "version": pkgMetadata.version}).Warning("could not parse provided file list")
+			// Even though there is an error, do not skip the package.
+			// The active vulnerability management analysis may be incomplete, but that should not prevent us from
+			// performing typical vulnerability management analysis.
+		}
+	}
+
+	key := featurefmt.PackageKey{
+		Name:    pkgName,
+		Version: pkgVersion,
+	}
+
+	// If the package already exists, then this must be a source package
+	// with multiple associated packages.
+	if feature, exists := packagesMap[key]; exists {
+		// Append the executable files for the associated package to the source package.
+		feature.ProvidedExecutables = append(feature.ProvidedExecutables, executables...)
+		return
+	}
+
+	packagesMap[key] = &database.FeatureVersion{
+		Feature: database.Feature{
+			Name: pkgName,
+		},
+		Version:             pkgVersion,
+		ProvidedExecutables: executables,
+	}
+}
 
 func (l lister) ListFeatures(files tarutil.FilesMap) ([]database.FeatureVersion, error) {
 	// Create a map to store packages and ensure their uniqueness
