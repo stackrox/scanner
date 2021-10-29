2e9328e61448ce4e168fdd72db0f616ffb862e6e
Frederico Bittencourt <fbittenc@redhat.com>
2021-10-29 09:30:00 +0200

    ci: dump info

--- image/scanner/scripts/entrypoint.sh
+++ image/scanner/scripts/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-set -euo pipefail
+set -xeuo pipefail
+
+find /.init-dirs -ls || true
+find /etc/pki/ca-trust -ls || true
+find /etc/ssl/certs -ls || true
 
 /restore-all-dir-contents
 /import-additional-cas
