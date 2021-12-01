3cc41151ff8cadd5e03ffdc5b60bd5c8ca5c8c77
RTann <rtann@redhat.com>
2021-12-01 13:32:34 -0800

    filter out invalid products

--- cpe/nvdtoolscache/load.go
+++ cpe/nvdtoolscache/load.go
@@ -67,7 +67,13 @@ func cpeIsApplicationOrLinuxKernel(cpe string) bool {
 	if len(spl) < 6 {
 		return false
 	}
-	return spl[2] == "a" || (spl[2] == "o" && spl[3] == "linux" && spl[4] == "linux_kernel")
+	// Check if the application is valid
+	if spl[2] == "a" && spl[3] != "" && spl[3] != "*" {
+		return true
+	}
+
+	// Return true if this is a linux kernel CPE.
+	return spl[2] == "o" && spl[3] == "linux" && spl[4] == "linux_kernel"
 }
 
 func isNodeValid(node *schema.NVDCVEFeedJSON10DefNode) bool {
