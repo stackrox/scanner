c305f6496edb3e2cea5c6ccbac132fecb72178f6
Ross <rtann@redhat.com>
2021-12-14 12:52:37 -0800

    Manually add CVE-2021-45046 (#560)

--- pkg/vulnloader/nvdloader/manual.go
+++ pkg/vulnloader/nvdloader/manual.go
@@ -35,7 +35,7 @@ var manuallyEnrichedVulns = map[string]*schema.NVDCVEFeedJSON10DefCVEItem{
 						{
 							Cpe23Uri:              "cpe:2.3:a:apache:log4j:*:*:*:*:*:*:*:*",
 							VersionEndExcluding:   "2.15.0",
-							VersionStartIncluding: "2.0.1",
+							VersionStartIncluding: "2.0.0", // Red Hat says 2.0.0, and I trust them more.
 						},
 					},
 					Operator: "OR",
@@ -63,5 +63,66 @@ var manuallyEnrichedVulns = map[string]*schema.NVDCVEFeedJSON10DefCVEItem{
 		},
 		LastModifiedDate: "2021-12-13T00:00Z",
 		PublishedDate:    "2021-12-10T00:00Z",
+	},
+	"CVE-2021-45046": {
+		CVE: &schema.CVEJSON40{
+			CVEDataMeta: &schema.CVEJSON40CVEDataMeta{
+				ID: "CVE-2021-45046",
+			},
+			DataFormat:  "MITRE",
+			DataType:    "CVE",
+			DataVersion: "4.0",
+			Description: &schema.CVEJSON40Description{
+				DescriptionData: []*schema.CVEJSON40LangString{
+					{
+						Lang:  "en",
+						Value: `It was found that the fix to address CVE-2021-44228 in Apache Log4j 2.15.0 was incomplete in certain non-default configurations. This could allows attackers with control over Thread Context Map (MDC) input data when the logging configuration uses a non-default Pattern Layout with either a Context Lookup (for example, $${ctx:loginId}) or a Thread Context Map pattern (%X, %mdc, or %MDC) to craft malicious input data using a JNDI Lookup pattern resulting in a denial of service (DOS) attack. Log4j 2.15.0 restricts JNDI LDAP lookups to localhost by default. Note that previous mitigations involving configuration such as to set the system property log4j2.noFormatMsgLookup to true do NOT mitigate this specific vulnerability.`,
+					},
+				},
+			},
+			References: &schema.CVEJSON40References{
+				ReferenceData: []*schema.CVEJSON40Reference{
+					{
+						Name: "https://logging.apache.org/log4j/2.x/security.html",
+					},
+				},
+			},
+		},
+		Configurations: &schema.NVDCVEFeedJSON10DefConfigurations{
+			CVEDataVersion: "4.0",
+			Nodes: []*schema.NVDCVEFeedJSON10DefNode{
+				{
+					CPEMatch: []*schema.NVDCVEFeedJSON10DefCPEMatch{
+						{
+							Cpe23Uri:              "cpe:2.3:a:apache:log4j:*:*:*:*:*:*:*:*",
+							VersionEndExcluding:   "2.16.0",
+							VersionStartIncluding: "2.0.0", // Red Hat says 2.0.0, and I trust them more.
+						},
+					},
+					Operator: "OR",
+				},
+			},
+		},
+		Impact: &schema.NVDCVEFeedJSON10DefImpact{
+			BaseMetricV3: &schema.NVDCVEFeedJSON10DefImpactBaseMetricV3{
+				CVSSV3: &schema.CVSSV30{
+					AttackComplexity:      "HIGH",
+					AttackVector:          "NETWORK",
+					AvailabilityImpact:    "LOW",
+					BaseScore:             3.7,
+					ConfidentialityImpact: "NONE",
+					IntegrityImpact:       "NONE",
+					PrivilegesRequired:    "NONE",
+					Scope:                 "UNCHANGED",
+					UserInteraction:       "NONE",
+					VectorString:          "CVSS:3.1/AV:N/AC:H/PR:N/UI:N/S:U/C:N/I:N/A:L",
+					Version:               "3.1",
+				},
+				ExploitabilityScore: 2.2,
+				ImpactScore:         1.4,
+			},
+		},
+		LastModifiedDate: "2021-12-13T00:00Z",
+		PublishedDate:    "2021-12-13T00:00Z",
 	},
 }
