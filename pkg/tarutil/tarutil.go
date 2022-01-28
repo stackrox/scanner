ecf9a84a3e0475787212df9a6f36c9eca5157819
Cong Du <c-du@users.noreply.github.com>
2022-01-28 05:43:04 -0800

    Adventure time all 400

--- pkg/tarutil/tarutil.go
+++ pkg/tarutil/tarutil.go
@@ -36,7 +36,7 @@ import (
 
 const (
 	// DefaultMaxExtractableFileSizeMB is the default value for the max extractable file size.
-	DefaultMaxExtractableFileSizeMB = 500
+	DefaultMaxExtractableFileSizeMB = 400
 )
 
 var (
