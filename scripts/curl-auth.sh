0e430568d5e2b374817a5f9adcf8abd2e6f04a64
Connor Gorman <cgorman@stackrox.com>
2022-02-01 10:53:45 -0800

    m

--- scripts/curl-auth.sh
+++ scripts/curl-auth.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
+# Clean up any old certs
 rm -rf central-cert
 mkdir central-cert
-
 
 kubectl -n stackrox get secret central-tls -o json | jq -r '.data["cert.pem"] | @base64d' > central-cert/cert.pem
 kubectl -n stackrox get secret central-tls -o json | jq -r '.data["key.pem"] | @base64d' > central-cert/key.pem
