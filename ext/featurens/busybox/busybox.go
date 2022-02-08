04d000b9db7a6ad9a46c57c643005a3678064db6
Cong Du <c-du@users.noreply.github.com>
2022-02-08 14:28:40 -0800

    large

--- ext/featurens/busybox/busybox.go
+++ ext/featurens/busybox/busybox.go
@@ -18,6 +18,7 @@ import (
 	"regexp"
 	"strings"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stackrox/scanner/database"
 	"github.com/stackrox/scanner/ext/featurens"
 	"github.com/stackrox/scanner/ext/versionfmt/language"
@@ -76,6 +77,7 @@ func (detector) Detect(files tarutil.LayerFiles, options *featurens.DetectorOpti
 	if !bytes.Equal(busyboxData.Contents, sbData.Contents) {
 		return nil
 	}
+	log.Infof("busybox content %d, sb %d, address %v vs %v", len(busyboxData.Contents), len(sbData.Contents), &busyboxData.Contents, &sbData.Contents)
 
 	// Validate busybox binary and extract version.
 	var version = parseBusyBoxVersion(busyboxData.Contents)
