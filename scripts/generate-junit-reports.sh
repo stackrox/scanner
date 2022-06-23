391cc3494fb4ac0ef046995bd081019c26f58bbd
RTann <rtann@redhat.com>
2022-06-23 15:18:39 -0700

    add credits

--- scripts/generate-junit-reports.sh
+++ scripts/generate-junit-reports.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Copied from https://github.com/stackrox/stackrox/blob/master/scripts/generate-junit-reports.sh
+
 mkdir -p junit-reports
 
 go-junit-report <"test-output/test.log" >"junit-reports/report.xml"
