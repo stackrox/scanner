0039bf307621ae781d6ab5cb3dba29aba6e2d97f
RTann <rtann@redhat.com>
2022-06-23 15:37:53 -0700

    add junit to e2e-tests

--- scripts/ci/jobs/e2e-tests.sh
+++ scripts/ci/jobs/e2e-tests.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+source "$ROOT/scripts/ci/lib.sh"
+
 set -euo pipefail
 
-make e2e-tests
+e2e_tests() {
+    info "Starting e2e tests"
+
+    make e2e-tests || touch FAIL
+
+    info "Saving junit XML report"
+    make generate-junit-reports || touch FAIL
+    store_test_results junit-reports reports
+
+    [[ ! -f FAIL ]] || die "E2E tests failed"
+}
+
+e2e_tests "$*"
