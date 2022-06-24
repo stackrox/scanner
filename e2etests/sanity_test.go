c5091f14afe5aaa032a6f948b7a3cbbc301d2ac5
RTann <rtann@redhat.com>
2022-06-24 14:44:06 -0700

    testing more

--- e2etests/sanity_test.go
+++ e2etests/sanity_test.go
@@ -130,12 +130,12 @@ func TestImageSanity(t *testing.T) {
 
 	for _, testCase := range testCases {
 		t.Run(testCase.image, func(t *testing.T) {
-			rUsername := os.Getenv("REDHAT_USERNAME") != ""
-			rPass := os.Getenv("REDHAT_PASSWORD") != ""
-			assert.True(t, rUsername)
-			assert.True(t, rPass)
-			gPass := os.Getenv("GOOGLE_SA_CIRCLECI_SCANNER") != ""
-			assert.True(t, gPass)
+			rUsername := os.Getenv("REDHAT_USERNAME") == ""
+			rPass := os.Getenv("REDHAT_PASSWORD") == ""
+			assert.True(t, rUsername, "Red Hat Username is actually set")
+			assert.True(t, rPass, "Red Hat Password is actually set")
+			gPass := os.Getenv("GOOGLE_SA_CIRCLECI_SCANNER") == ""
+			assert.True(t, gPass, "Google Password is actually set")
 			verifyImageHasExpectedFeatures(t, cli, testCase, &types.ImageRequest{Image: testCase.image, Registry: testCase.registry, UncertifiedRHELScan: testCase.uncertifiedRHEL})
 		})
 	}
