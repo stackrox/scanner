ebc20bf817b38ce02aab9b6ad70f03633c3351a1
RTann <rtann@redhat.com>
2022-06-27 11:49:57 -0700

    i keep forgetting to change this i promise you can still review this PR though

--- .openshift-ci/ci_tests.py
+++ .openshift-ci/ci_tests.py
@@ -40,6 +40,6 @@ class E2ETest(BaseTest):
         print("Executing E2E tests")
 
         self.run_with_graceful_kill(
-            ["scripts/ci/e2e-tests.sh"],
+            ["scripts/ci/jobs/e2etests/e2e-tests.sh"],
             E2ETest.TEST_TIMEOUT,
         )
