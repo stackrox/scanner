45f4c9ed8bb5a4af4245989b280c1c63f0cab468
RTann <rtann@redhat.com>
2022-06-28 15:10:29 -0700

    PR update

--- scripts/ci/jobs/scale_tests.py
+++ scripts/ci/jobs/scale_tests.py
@@ -3,18 +3,11 @@
 """
 Run E2E tests in a GKE cluster
 """
-import os
 from pre_tests import PreE2ETests
 from ci_tests import ScaleTest
 from post_tests import PostClusterTest, FinalPost
 from clusters import GKECluster
 from runners import ClusterTestRunner
-
-# set required test parameters
-os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
-
-# override default test environment
-os.environ["LOAD_BALANCER"] = "lb"
 
 ClusterTestRunner(
     cluster=GKECluster("scale-tests"),
