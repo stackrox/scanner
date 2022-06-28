211f9e902bab9013a9e0a3884ca53a0fc43706ab
J. Victor Martins <jvdm@sdf.org>
2022-06-28 15:33:17 -0700

    feat: Add support for RHEL9 images (#752)

--- pkg/rhelv2/rpm/rpm_language_analyzer.go
+++ pkg/rhelv2/rpm/rpm_language_analyzer.go
@@ -1,12 +1,9 @@
 package rpm
 
 import (
-	"os"
-	"os/exec"
-
-	log "github.com/sirupsen/logrus"
 	"github.com/stackrox/rox/pkg/stringutils"
 	"github.com/stackrox/scanner/pkg/component"
+	"github.com/stackrox/scanner/pkg/rpm"
 	"github.com/stackrox/scanner/pkg/tarutil"
 )
 
@@ -16,16 +13,14 @@ func AnnotateComponentsWithPackageManagerInfo(files tarutil.LayerFiles, componen
 	if len(components) == 0 {
 		return nil
 	}
-	f, hasFile := files.Get(dbPath)
-	if !hasFile {
-		return nil
-	}
-	matcher, finish, err := isProvidedByRPMPackageMatcher(f.Contents)
+	rpmDB, err := rpm.CreateDatabaseFromLayer(files)
 	if err != nil {
 		return err
 	}
-	defer finish()
-
+	if rpmDB == nil {
+		return nil
+	}
+	defer rpmDB.Delete()
 	locationAlreadyChecked := make(map[string]bool)
 	for _, c := range components {
 		// This handles jar-in-jar cases as the location is manually created so we only want
@@ -36,63 +31,8 @@ func AnnotateComponentsWithPackageManagerInfo(files tarutil.LayerFiles, componen
 			c.FromPackageManager = fromPackageManager
 			continue
 		}
-		c.FromPackageManager = matcher(normalizedLocation)
+		c.FromPackageManager = rpmDB.ProvidesFile(normalizedLocation)
 		locationAlreadyChecked[normalizedLocation] = c.FromPackageManager
 	}
 	return nil
-}
-
-// isProvidedByRPMPackageMatcher uses the given package contents (expected to be an RPM Berkeley DB)
-// to return:
-// * a function which returns if the given file path is provided by an RPM package.
-// * a function to be called once the package contents are no longer needed which cleans up any used resources.
-// * an error.
-func isProvidedByRPMPackageMatcher(packagesContents []byte) (func(string) bool, func(), error) {
-	if packagesContents == nil {
-		// Default return always says the given path is not provided by an RPM package.
-		return func(string) bool { return false }, func() {}, nil
-	}
-
-	// Write the required "Packages" file to disk
-	tmpDir, err := os.MkdirTemp("", "rpm")
-	if err != nil {
-		log.WithError(err).Error("could not create temporary folder for RPM detection")
-		return nil, nil, err
-	}
-
-	err = os.WriteFile(tmpDir+"/Packages", packagesContents, 0700)
-	if err != nil {
-		log.WithError(err).Error("could not create temporary file for RPM detection")
-		return nil, nil, err
-	}
-
-	finishFn := func() { _ = os.RemoveAll(tmpDir) }
-
-	return func(path string) bool {
-		// We need the full path of the file.
-		// When we originally extract the file, the `/` prefix is removed.
-		// Add it back here.
-		fullPath := "/" + path
-
-		cmd := exec.Command("rpm",
-			`--dbpath`, tmpDir,
-			`-q`, `--whatprovides`, fullPath)
-
-		if err := cmd.Run(); err != nil {
-			// When an RPM package does not provide a file, the expected output has
-			// status code 1.
-			if exitError, ok := err.(*exec.ExitError); ok && exitError.ExitCode() == 1 {
-				// RPM does NOT provide this package.
-				return false
-			}
-
-			log.WithError(err).Errorf("unexpected error when determining if %s belongs to an RPM package", fullPath)
-			// Upon error, say no RPM package provides this file.
-			return false
-		}
-
-		// The command exited properly, which implies the file IS provided by an RPM package.
-		return true
-
-	}, finishFn, nil
 }
--- pkg/rhelv2/rpm/rpm_test.go
+++ pkg/rhelv2/rpm/rpm_test.go
@@ -1,94 +1,296 @@
 package rpm
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
 
 	"github.com/stackrox/scanner/database"
+	"github.com/stackrox/scanner/pkg/features"
 	"github.com/stackrox/scanner/pkg/tarutil"
 	"github.com/stackrox/scanner/pkg/testutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-// ListFeaturesTest does the same as ListFeatures but should only be used for testing.
-func ListFeaturesTest(files tarutil.LayerFiles) ([]*database.RHELv2Package, []string, error) {
-	return listFeatures(files, queryFmtTest)
+var testDirectory string
+
+func init() {
+	_, filename, _, _ := runtime.Caller(0)
+	testDirectory = filepath.Dir(filename)
 }
 
-func TestRPMFeatureDetection(t *testing.T) {
-	sampleExpectedPkgs := []*database.RHELv2Package{
+// ListFeaturesTest does the same as ListFeatures but should only be used for testing.
+func ListFeaturesTest(files tarutil.LayerFiles) ([]*database.RHELv2Package, []string, error) {
+	return listFeatures(files, true)
+}
+
+func Test_listFeatures(t *testing.T) {
+	type args struct {
+		layerFiles tarutil.LayerFiles
+		queryFmt   string
+	}
+	tests := []struct {
+		name string
+		args args
+
+		// Add test layer files from a map of file data.
+		files map[string]tarutil.FileData
+		// Add test layer files from a map of files stored in the testdata/
+		// directory.
+		filesFromTestData map[string]string
+		// Sets the queryFmt to test base on active vulnerability flag.
+		isActiveVulnMgmt bool
+
+		// Expected values for assertion.
+		sampleExpectedPkgs []*database.RHELv2Package
+		unexpectedPkgs     []*database.RHELv2Package
+		expectedCPEs       []string
+		wantErr            assert.ErrorAssertionFunc
+		requireRHEL9       bool
+	}{
 		{
-			Name:    "zlib",
-			Version: "1.2.11-16.el8_2",
-			Arch:    "x86_64",
-			ExecutableToDependencies: database.StringToStringsMap{
-				"/usr/lib64/libz.so.1":      {},
-				"/usr/lib64/libz.so.1.2.11": {},
+			name: "TestRPMFeatureDetection with BerkeleyDB",
+			filesFromTestData: map[string]string{
+				"root/buildinfo/content_manifests/test.json": "test.json",
+				"var/lib/rpm/Packages":                       "Packages",
+			},
+			isActiveVulnMgmt: false,
+			wantErr:          assert.NoError,
+			sampleExpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "zlib",
+					Version: "1.2.11-16.el8_2",
+					Arch:    "x86_64",
+				},
+				{
+					Name:    "dbus-common",
+					Version: "1:1.12.8-12.el8_3",
+					Arch:    "noarch",
+				},
+				{
+					Name:    "ncurses-libs",
+					Version: "6.1-7.20180224.el8",
+					Arch:    "x86_64",
+				},
+				{
+					Name:    "redhat-release",
+					Version: "8.3-1.0.el8",
+					Arch:    "x86_64",
+				},
+			},
+			unexpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "gpg-pubkey",
+					Version: "d4082792-5b32db75",
+				},
+			},
+			expectedCPEs: []string{
+				"cpe:/o:redhat:enterprise_linux:8::baseos",
+				"cpe:/a:redhat:enterprise_linux:8::appstream",
 			},
 		},
 		{
-			Name:    "dbus-common",
-			Version: "1:1.12.8-12.el8_3",
-			Arch:    "noarch",
-		},
-		{
-			Name:    "ncurses-libs",
-			Version: "6.1-7.20180224.el8",
-			Arch:    "x86_64",
-			ExecutableToDependencies: database.StringToStringsMap{
-				"/usr/lib64/libform.so.6":       {},
-				"/usr/lib64/libncursesw.so.6.1": {},
-				"/usr/lib64/libpanelw.so.6":     {},
+			name: "TestRPMFeatureDetectionWithActiveVulnMgmt with BerkleyDB",
+			files: map[string]tarutil.FileData{
+				"usr/lib64/libz.so.1":          {Executable: true},
+				"usr/lib64/libz.so.1.2.11":     {Executable: true},
+				"usr/lib64/libform.so.6":       {Executable: true},
+				"usr/lib64/libncursesw.so.6.1": {Executable: true},
+				"usr/lib64/libpanelw.so.6":     {Executable: true},
+				"etc/redhat-release":           {Executable: true},
+				"etc/os-release":               {Executable: true},
+				"usr/lib/redhat-release":       {Executable: true},
+			},
+			filesFromTestData: map[string]string{
+				"root/buildinfo/content_manifests/test.json": "test.json",
+				"var/lib/rpm/Packages":                       "Packages",
+			},
+			isActiveVulnMgmt: true,
+			wantErr:          assert.NoError,
+			sampleExpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "zlib",
+					Version: "1.2.11-16.el8_2",
+					Arch:    "x86_64",
+					ExecutableToDependencies: database.StringToStringsMap{
+						"/usr/lib64/libz.so.1":      {},
+						"/usr/lib64/libz.so.1.2.11": {},
+					},
+				},
+				{
+					Name:    "dbus-common",
+					Version: "1:1.12.8-12.el8_3",
+					Arch:    "noarch",
+				},
+				{
+					Name:    "ncurses-libs",
+					Version: "6.1-7.20180224.el8",
+					Arch:    "x86_64",
+					ExecutableToDependencies: database.StringToStringsMap{
+						"/usr/lib64/libform.so.6":       {},
+						"/usr/lib64/libncursesw.so.6.1": {},
+						"/usr/lib64/libpanelw.so.6":     {},
+					},
+				},
+				{
+					Name:    "redhat-release",
+					Version: "8.3-1.0.el8",
+					Arch:    "x86_64",
+				},
+			},
+			unexpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "gpg-pubkey",
+					Version: "d4082792-5b32db75",
+				},
+			},
+			expectedCPEs: []string{
+				"cpe:/o:redhat:enterprise_linux:8::baseos",
+				"cpe:/a:redhat:enterprise_linux:8::appstream",
 			},
 		},
 		{
-			Name:    "redhat-release",
-			Version: "8.3-1.0.el8",
-			Arch:    "x86_64",
+			name: "TestRPMFeatureDetection with SQLite",
+			filesFromTestData: map[string]string{
+				"root/buildinfo/content_manifests/test.json": "test.json",
+				"var/lib/rpm/rpmdb.sqlite":                   "rpmdb.sqlite",
+			},
+			isActiveVulnMgmt: false,
+			requireRHEL9:     true,
+			wantErr:          assert.NoError,
+			sampleExpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "zlib",
+					Version: "1.2.11-16.el8_2",
+					Arch:    "x86_64",
+				},
+				{
+					Name:    "dbus-common",
+					Version: "1:1.12.8-12.el8_3",
+					Arch:    "noarch",
+				},
+				{
+					Name:    "ncurses-libs",
+					Version: "6.1-7.20180224.el8",
+					Arch:    "x86_64",
+				},
+				{
+					Name:    "redhat-release",
+					Version: "8.3-1.0.el8",
+					Arch:    "x86_64",
+				},
+			},
+			unexpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "gpg-pubkey",
+					Version: "d4082792-5b32db75",
+				},
+			},
+			expectedCPEs: []string{
+				"cpe:/o:redhat:enterprise_linux:8::baseos",
+				"cpe:/a:redhat:enterprise_linux:8::appstream",
+			},
 		},
-	}
-
-	unexpectedPkgs := []*database.RHELv2Package{
 		{
-			Name:    "gpg-pubkey",
-			Version: "d4082792-5b32db75",
+			name: "TestRPMFeatureDetectionWithActiveVulnMgmt with SQLite",
+			files: map[string]tarutil.FileData{
+				"usr/lib64/libz.so.1":          {Executable: true},
+				"usr/lib64/libz.so.1.2.11":     {Executable: true},
+				"usr/lib64/libform.so.6":       {Executable: true},
+				"usr/lib64/libncursesw.so.6.1": {Executable: true},
+				"usr/lib64/libpanelw.so.6":     {Executable: true},
+				"etc/redhat-release":           {Executable: true},
+				"etc/os-release":               {Executable: true},
+				"usr/lib/redhat-release":       {Executable: true},
+			},
+			filesFromTestData: map[string]string{
+				"root/buildinfo/content_manifests/test.json": "test.json",
+				"var/lib/rpm/rpmdb.sqlite":                   "rpmdb.sqlite",
+			},
+			isActiveVulnMgmt: true,
+			requireRHEL9:     true,
+			wantErr:          assert.NoError,
+			sampleExpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "zlib",
+					Version: "1.2.11-16.el8_2",
+					Arch:    "x86_64",
+					ExecutableToDependencies: database.StringToStringsMap{
+						"/usr/lib64/libz.so.1":      {},
+						"/usr/lib64/libz.so.1.2.11": {},
+					},
+				},
+				{
+					Name:    "dbus-common",
+					Version: "1:1.12.8-12.el8_3",
+					Arch:    "noarch",
+				},
+				{
+					Name:    "ncurses-libs",
+					Version: "6.1-7.20180224.el8",
+					Arch:    "x86_64",
+					ExecutableToDependencies: database.StringToStringsMap{
+						"/usr/lib64/libform.so.6":       {},
+						"/usr/lib64/libncursesw.so.6.1": {},
+						"/usr/lib64/libpanelw.so.6":     {},
+					},
+				},
+				{
+					Name:    "redhat-release",
+					Version: "8.3-1.0.el8",
+					Arch:    "x86_64",
+				},
+			},
+			unexpectedPkgs: []*database.RHELv2Package{
+				{
+					Name:    "gpg-pubkey",
+					Version: "d4082792-5b32db75",
+				},
+			},
+			expectedCPEs: []string{
+				"cpe:/o:redhat:enterprise_linux:8::baseos",
+				"cpe:/a:redhat:enterprise_linux:8::appstream",
+			},
 		},
 	}
 
-	expectedCPEs := []string{
-		"cpe:/o:redhat:enterprise_linux:8::baseos",
-		"cpe:/a:redhat:enterprise_linux:8::appstream",
+	for _, tt := range tests {
+		if tt.requireRHEL9 && !features.RHEL9Scanning.Enabled() {
+			continue
+		}
+		// Initialize test arguments.
+		if tt.filesFromTestData != nil {
+			if tt.files == nil {
+				tt.files = make(map[string]tarutil.FileData)
+			}
+			for n, f := range tt.filesFromTestData {
+				c, err := os.ReadFile(filepath.Join(testDirectory, "testdata", f))
+				require.NoError(t, err)
+				tt.files[n] = tarutil.FileData{Contents: c}
+			}
+		}
+		tt.args.layerFiles = tarutil.CreateNewLayerFiles(tt.files)
+
+		// Run test.
+		t.Run(tt.name, func(t *testing.T) {
+			envIsolator := testutils.NewEnvIsolator(t)
+			defer envIsolator.RestoreAll()
+			envIsolator.Setenv("REPO_TO_CPE_DIR", filepath.Join(testDirectory, "/testdata"))
+			tt.args.layerFiles = tarutil.CreateNewLayerFiles(tt.files)
+
+			// Functions call.
+			pkgs, cpes, err := listFeatures(tt.args.layerFiles, true)
+
+			// Assertions.
+			if !tt.wantErr(t, err, fmt.Sprintf("listFeatures(%v, %v)", tt.args.layerFiles, tt.args.queryFmt)) {
+				return
+			}
+			assert.ElementsMatch(t, cpes, tt.expectedCPEs)
+			assert.Subset(t, pkgs, tt.sampleExpectedPkgs)
+			assert.NotSubset(t, pkgs, tt.unexpectedPkgs)
+		})
 	}
-
-	_, filename, _, _ := runtime.Caller(0)
-	d, err := os.ReadFile(filepath.Join(filepath.Dir(filename), "/testdata/Packages"))
-	require.NoError(t, err)
-
-	manifest, err := os.ReadFile(filepath.Join(filepath.Dir(filename), "/testdata/test.json"))
-	require.NoError(t, err)
-
-	envIsolator := testutils.NewEnvIsolator(t)
-	defer envIsolator.RestoreAll()
-	cpesDir := filepath.Join(filepath.Dir(filename), "/testdata")
-	envIsolator.Setenv("REPO_TO_CPE_DIR", cpesDir)
-
-	pkgs, cpes, err := ListFeaturesTest(tarutil.CreateNewLayerFiles(map[string]tarutil.FileData{
-		"var/lib/rpm/Packages":                       {Contents: d},
-		"root/buildinfo/content_manifests/test.json": {Contents: manifest},
-		"usr/lib64/libz.so.1":                        {Executable: true},
-		"usr/lib64/libz.so.1.2.11":                   {Executable: true},
-		"usr/lib64/libform.so.6":                     {Executable: true},
-		"usr/lib64/libncursesw.so.6.1":               {Executable: true},
-		"usr/lib64/libpanelw.so.6":                   {Executable: true},
-		"etc/redhat-release":                         {Executable: true},
-		"etc/os-release":                             {Executable: true},
-		"usr/lib/redhat-release":                     {Executable: true},
-	}))
-	assert.NoError(t, err)
-	assert.ElementsMatch(t, cpes, expectedCPEs)
-	assert.Subset(t, pkgs, sampleExpectedPkgs)
-	assert.NotSubset(t, pkgs, unexpectedPkgs)
 }
