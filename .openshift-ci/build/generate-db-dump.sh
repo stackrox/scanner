31d04919e4646bb0e6d98cfcf684448bec3967c6
RTann <rtannenb@redhat.com>
2022-07-19 12:59:05 -0700

    update

--- .openshift-ci/build/generate-db-dump.sh
+++ .openshift-ci/build/generate-db-dump.sh
@@ -16,8 +16,6 @@ generate_db_dump() {
     mkdir /tmp/postgres
     pg_dump -U postgres postgres://127.0.0.1:5432 > /tmp/postgres/pg-definitions.sql
     gzip --best /tmp/postgres/pg-definitions.sql
-
-    store_artifacts /tmp/postgres postgres
 }
 
 generate_db_dump "$*"
