0ae1de0f16bda4b7b63f9a87fddc025de922d31e
RTann <rtannenb@redhat.com>
2022-07-19 16:54:44 -0700

    this is so annoying

--- .openshift-ci/build/generate-db-dump.sh
+++ .openshift-ci/build/generate-db-dump.sh
@@ -6,18 +6,22 @@ source "$ROOT/scripts/ci/postgres.sh"
 
 set -euo pipefail
 
+openshift_ci_mods
+
+gate_job generate-db-dump
+
 generate_db_dump() {
     touch /tmp/hold
 
     info "Generating DB dump"
 
-    start_postgres
-
-    "$ROOT/bin/updater" load-dump --postgres-host 127.0.0.1 --postgres-port 5432 --dump-file /tmp/genesis-dump/genesis-dump.zip
-
-    mkdir /tmp/postgres
-    pg_dump -U postgres postgres://127.0.0.1:5432 > /tmp/postgres/pg-definitions.sql
-    gzip --best /tmp/postgres/pg-definitions.sql
+#    start_postgres
+#
+#    "$ROOT/bin/updater" load-dump --postgres-host 127.0.0.1 --postgres-port 5432 --dump-file /tmp/genesis-dump/genesis-dump.zip
+#
+#    mkdir /tmp/postgres
+#    pg_dump -U postgres postgres://127.0.0.1:5432 > /tmp/postgres/pg-definitions.sql
+#    gzip --best /tmp/postgres/pg-definitions.sql
 }
 
 generate_db_dump "$*"
