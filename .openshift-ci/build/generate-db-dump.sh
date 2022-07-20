a87615fdd5381835a6e1a4e7fb7fc792969efd40
RTann <rtannenb@redhat.com>
2022-07-19 18:40:27 -0700

    pls

--- .openshift-ci/build/generate-db-dump.sh
+++ .openshift-ci/build/generate-db-dump.sh
@@ -15,12 +15,15 @@ generate_db_dump() {
 
     info "Generating DB dump"
 
-    start_postgres
+    groupadd -g 71 pg
+    adduser pg -u 71 -g 71 -d /var/lib/postgresql -s /bin/sh
+
+    runuser -l pg -c start_postgres
 
     "$ROOT/bin/updater" load-dump --postgres-host 127.0.0.1 --postgres-port 5432 --dump-file /tmp/genesis-dump/genesis-dump.zip
 
     mkdir /tmp/postgres
-    pg_dump -U postgres postgres://127.0.0.1:5432 > /tmp/postgres/pg-definitions.sql
+    runuser -l pg -c 'pg_dump -U postgres postgres://127.0.0.1:5432 > /tmp/postgres/pg-definitions.sql'
     gzip --best /tmp/postgres/pg-definitions.sql
 }
 
