039e0dcdcf7c9ad7fe687762adc68360d08679b5
RTann <rtannenb@redhat.com>
2022-09-12 11:27:42 -0700

    for now

--- scripts/ci/jobs/upload-dumps-for-embedding.sh
+++ scripts/ci/jobs/upload-dumps-for-embedding.sh
@@ -7,19 +7,20 @@ source "$ROOT/scripts/lib.sh"
 set -euo pipefail
 
 upload_dumps_for_embedding() {
-    if is_in_PR_context; then
-        info "In PR context. Skipping..."
-        return 0
+    local base_ref
+    base_ref="$(get_base_ref)"
+    if [[ "${base_ref}" == "master" ]]; then
+        info "Starting dumps upload"
+
+        setup_gcp
+
+        info "Uploading dumps"
+        gsutil cp /tmp/vuln-dump/nvd-definitions.zip gs://stackrox-scanner-ci-vuln-dump
+        gsutil cp /tmp/vuln-dump/k8s-definitions.zip gs://stackrox-scanner-ci-vuln-dump
+        gsutil cp /tmp/vuln-dump/repo2cpe.zip gs://stackrox-scanner-ci-vuln-dump
+    else
+        info "This is not the master branch, so skipping..."
     fi
-
-    info "Starting dumps upload"
-
-    setup_gcp
-
-    info "Uploading dumps"
-    gsutil cp /tmp/vuln-dump/nvd-definitions.zip gs://stackrox-scanner-ci-vuln-dump
-    gsutil cp /tmp/vuln-dump/k8s-definitions.zip gs://stackrox-scanner-ci-vuln-dump
-    gsutil cp /tmp/vuln-dump/repo2cpe.zip gs://stackrox-scanner-ci-vuln-dump
 }
 
 upload_dumps_for_embedding "$*"
