8b08c6e2983a2702eec7421ca64e2e774de574f1
daynewlee <yli3@redhat.com>
2022-10-19 10:12:53 -0500

    Added GetIstioVulnerabilities

--- api/v1/orchestratorscan/service.go
+++ api/v1/orchestratorscan/service.go
@@ -12,6 +12,7 @@ import (
 	"github.com/stackrox/scanner/api/v1/convert"
 	"github.com/stackrox/scanner/database"
 	v1 "github.com/stackrox/scanner/generated/scanner/api/v1"
+	istioCache "github.com/stackrox/scanner/istio/cache"
 	k8scache "github.com/stackrox/scanner/k8s/cache"
 	"github.com/stackrox/scanner/pkg/version"
 	"google.golang.org/grpc"
@@ -27,18 +28,20 @@ type Service interface {
 }
 
 // NewService returns the service for scanning
-func NewService(db database.Datastore, k8sCache k8scache.Cache) Service {
+func NewService(db database.Datastore, k8sCache k8scache.Cache, istioCache istioCache.Cache) Service {
 	return &serviceImpl{
-		version:  version.Version,
-		db:       db,
-		k8sCache: k8sCache,
+		version:    version.Version,
+		db:         db,
+		k8sCache:   k8sCache,
+		istioCache: istioCache,
 	}
 }
 
 type serviceImpl struct {
-	version  string
-	db       database.Datastore
-	k8sCache k8scache.Cache
+	version    string
+	db         database.Datastore
+	k8sCache   k8scache.Cache
+	istioCache istioCache.Cache
 }
 
 func filterInvalidVulns(vulns []*v1.Vulnerability) []*v1.Vulnerability {
@@ -98,6 +101,39 @@ func (s *serviceImpl) GetKubeVulnerabilities(_ context.Context, req *v1.GetKubeV
 	}
 
 	resp.GenericVulnerabilities, err = s.getKubernetesVuln(k8scache.Generic, req.GetKubernetesVersion())
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return resp, nil
+}
+
+func (s *serviceImpl) getIstioVuln(version string) ([]*v1.Vulnerability, error) {
+	if version == "" {
+		return nil, errors.New("Can't get vulnerabilities for empty version.")
+	}
+	version, err := convert.TruncateVersion(version)
+	if err != nil {
+		log.Warnf("Unable to convert version of %s - %v. Skipping...", version, err)
+		return nil, nil
+	}
+
+	vulns := s.istioCache.GetVulnsByVersion(version)
+	converted, err := convert.IstioVulnerabilities(version, vulns)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to convert vulnerabilities")
+	}
+	return filterInvalidVulns(converted), nil
+}
+
+// GetIstioVulnerabilities returns Istio vulnerabilities for requested Kubernetes version.
+func (s *serviceImpl) GetIstioVulnerabilities(_ context.Context, req *v1.GetIstioVulnerabilitiesRequest) (*v1.GetIstioVulnerabilitiesResponse, error) {
+	var err error
+	resp := &v1.GetIstioVulnerabilitiesResponse{
+		ScannerVersion: s.version,
+	}
+
+	resp.Vulnerabilities, err = s.getIstioVuln(req.GetIstioVersion())
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
