cd1d64d69072b3358f1f8ba5f21e99c8c521b786
RTann <rtannenb@redhat.com>
2022-11-01 19:58:54 -0700

    update

--- e2etests/testcase_test.go
+++ e2etests/testcase_test.go
@@ -3825,7 +3825,7 @@ Applications using RegexRequestMatcher with '.' in the regular expression are po
 				NamespaceName: "ubuntu:22.10",
 				VersionFormat: "dpkg",
 				Version:       "3.0.5-2ubuntu2",
-				FixedBy: "3.0.5-2ubuntu2",
+				FixedBy:       "3.0.5-2ubuntu2",
 				// TODO: Why is it this layer?
 				AddedBy: "sha256:301a8b74f71f85f3a31e9c7e7fedd5b001ead5bcf895bc2911c1d260e06bd987",
 			},
