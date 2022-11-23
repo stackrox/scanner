e0e284217f58c151b1f68cbd80f07ce6525efb6a
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2022-11-23 17:47:53 +0100

    Enable the ff and see what CI says

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -6,5 +6,5 @@ var (
 	// RHEL9Scanning enables support for scanning RHEL9-based images.
 	RHEL9Scanning = registerFeature("Enable support for scanning RHEL9 images", "ROX_RHEL9_SCANNING", true)
 	// RHCOSNodeScanning enables phase 1 functions of "Full host level vulnerability scanning for RHCOS nodes" (ROX-10818)
-	RHCOSNodeScanning = registerFeature("Enable RHCOS node scanning of OS and installed packages", "ROX_RHCOS_NODE_SCANNING", false)
+	RHCOSNodeScanning = registerFeature("Enable RHCOS node scanning of OS and installed packages", "ROX_RHCOS_NODE_SCANNING", true)
 )
