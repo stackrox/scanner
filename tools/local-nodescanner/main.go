cb135bb09d11898b53a7fe5cf4c1b082490f78cf
Matthias Meidinger <mmeiding@redhat.com>
2022-12-01 15:48:08 +0100

    Introduce local nodescanner

---
+++ tools/local-nodescanner/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stackrox/scanner/pkg/analyzer/nodes"
+)
+
+// local nodescanner is an application that allows you to run the node scan / inventory code locally on you machine.
+
+// Required:
+// Extracted filesystem from an RHCOS live .ISO (https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/)
+
+// Howto:
+// Download the ISO, extract with 7z x <iso-name>, then extract images/pxeboot/root.squashfs with 7z
+// The root fs will be in ostree/deploy/rhcos/deploy/
+
+func main() {
+	var uncertifiedRHEL bool
+	var path string
+
+	flag.StringVar(&path, "path", "", "Path to an extracted RHCOS rootFS folder")
+	flag.BoolVar(&uncertifiedRHEL, "uncertifiedRHEL", false, "Whether to treat this run as uncertified RHEL FS")
+	flag.Parse()
+
+	logrus.Infof("%v", path)
+	if path == "" {
+		logrus.Fatal("A valid path is required")
+	}
+
+	components, err := nodes.Analyze("localnode", path, uncertifiedRHEL)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Infof("Nodescan finished. Returned components: %v", components)
+}
