1a8f28805d0659acf4e80d3596a87fcb7befe938
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-01-27 11:47:57 +0100

    Add FF for ROX_RHCOS_NODE_SCANNING

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -5,4 +5,6 @@ var (
 	ContinueUnknownOS = registerFeature("Enable continuation upon detecting unknown OS", "ROX_CONTINUE_UNKNOWN_OS", true)
 	// RHEL9Scanning enables support for scanning RHEL9-based images.
 	RHEL9Scanning = registerFeature("Enable support for scanning RHEL9 images", "ROX_RHEL9_SCANNING", true)
+	// RHCOSNodeScanning enables phase 1 functions of "Full host level vulnerability scanning for RHCOS nodes" (ROX-10818)
+	RHCOSNodeScanning = registerFeature("Enable RHCOS node scanning of OS and installed packages", "ROX_RHCOS_NODE_SCANNING", false)
 )
