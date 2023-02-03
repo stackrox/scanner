227fe5f4c75c2de1671f762fc1a803cc9d5bc7f0
Yi Li <yli3@redhat.com>
2023-02-03 16:01:00 -0600

    ROX-13796: benchmark node analyze (#1073)

---
+++ benchmarks/analyzeNode/analyze_node_test.go
@@ -0,0 +1,24 @@
+package detectconent
+
+import (
+	"runtime"
+	"testing"
+
+	node "github.com/stackrox/scanner/pkg/analyzer/nodes"
+	// Register the Docker image extractor
+	_ "github.com/stackrox/scanner/ext/imagefmt/docker"
+)
+
+func BenchmarkAnalyzeNode(b *testing.B) {
+	runBenchmarkAnalyzeNode(b, "/local/path/to/file/system")
+}
+
+func runBenchmarkAnalyzeNode(b *testing.B, pathName string) {
+	runtime.GC()
+
+	for i := 0; i < b.N; i++ {
+		node.Analyze("testNode", pathName, node.AnalyzeOpts{UncertifiedRHEL: false, IsRHCOSRequired: true})
+	}
+	// Memory measuring command: go test -bench=foo -benchmem
+
+}
