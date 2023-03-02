339a645ac659f22a56e56408cd508f29811ff5f0
RTann <rtannenb@redhat.com>
2023-03-02 10:56:01 -0800

    update

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -780,7 +780,7 @@ send_slack_notice_for_vuln_check_failure() {
 
     local repo="scanner"
     local job_name="sanity-check-vuln-updates"
-    local mentions="@scanner-defs-oncall"
+    local mentions="@acs-scanner-primary"
     local log_url="https://prow.ci.openshift.org/view/gs/origin-ci-test/logs/${JOB_NAME}/${BUILD_ID}"
 
     # shellcheck disable=SC2016
