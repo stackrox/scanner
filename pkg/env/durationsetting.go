e89ac79f8d4c9e923bf43c6fe5ba94b8a2bb37f4
Matthias Meidinger <mmeiding@redhat.com>
2023-03-24 12:22:22 +0100

    WIP: Migrate

---
+++ pkg/env/durationsetting.go
@@ -0,0 +1,47 @@
+package env
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/stackrox/rox/pkg/utils"
+)
+
+// DurationSetting represents an environment variable which should be parsed into a duration
+type DurationSetting interface {
+	Setting
+}
+
+type durationSetting struct {
+	Setting
+}
+
+//// DurationSetting returns the Duration object represented by the environment variable
+//func (d *DurationSetting) DurationSetting() time.Duration {
+//	val := os.Getenv(d.envVar)
+//	if val != "" {
+//		dur, err := time.ParseDuration(val)
+//		if err == nil && validateDuration(dur, d.opts) == nil {
+//			return dur
+//		}
+//		log.Warnf("%s is not a valid environment variable for %s, using default value: %v", val, d.envVar, d.defaultDuration)
+//	}
+//	return d.defaultDuration
+//}
+
+func RegisterDurationSetting(envVar string, defaultDuration time.Duration, opts ...DurationSettingOption) *DurationSetting {
+
+	utils.CrashOnError(validateDuration(defaultDuration, opts))
+
+	return s
+}
+
+func validateDuration(d time.Duration, opts durationSettingOpts) error {
+	if d < 0 {
+		return fmt.Errorf("invalid duration: %v < 0", d)
+	}
+	if !opts.zeroAllowed && d == 0 {
+		return fmt.Errorf("invalid duration: %v == 0", d)
+	}
+	return nil
+}
---
+++ pkg/env/durationsetting_options.go
@@ -0,0 +1,25 @@
+package env
+
+type durationSettingOpts struct {
+	zeroAllowed bool
+	settingOptions
+}
+
+// DurationSettingOption represents an option which may be specified
+// for a DurationSetting environment variable.
+type DurationSettingOption interface {
+	SettingOption
+}
+
+type durationSettingOptsFunc func(opts *durationSettingOpts)
+
+func (f durationSettingOptsFunc) apply(opts *durationSettingOpts) {
+	f(opts)
+}
+
+// WithDurationZeroAllowed specifies the DurationSetting may have a value of 0.
+func WithDurationZeroAllowed() DurationSettingOption {
+	return durationSettingOptsFunc(func(opts *durationSettingOpts) {
+		opts.zeroAllowed = true
+	})
+}
