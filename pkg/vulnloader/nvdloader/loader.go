094b159c66fc5f86681c9a722a3fee560a23e1bf
David Caravello <119438707+dcaravel@users.noreply.github.com>
2023-07-05 14:04:39 -0500

    Add TEMP/debug statements back

--- pkg/vulnloader/nvdloader/loader.go
+++ pkg/vulnloader/nvdloader/loader.go
@@ -1,9 +1,11 @@
 package nvdloader
 
 import (
+	"bytes"
 	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -87,8 +89,11 @@ func downloadFeedForYear(enrichmentMap map[string]*FileFormatWrapper, outputDir 
 	}
 	defer utils.IgnoreError(resp.Body.Close)
 	// Un-gzip it.
-	gr, err := gzip.NewReader(resp.Body)
+	var buf bytes.Buffer
+	tee := io.TeeReader(resp.Body, &buf)
+	gr, err := gzip.NewReader(tee)
 	if err != nil {
+		fmt.Printf("\nContents of buf: %v\n\n", buf.String())
 		return errors.Wrapf(err, "couldn't read resp body for year %d", year)
 	}
 
