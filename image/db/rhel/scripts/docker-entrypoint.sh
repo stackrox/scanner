9bf0a3ff20a6bdc7f8744905a8c2b07ee7567563
Connor Gorman <cgorman@redhat.com>
2023-10-04 08:39:31 -0700

    Use smart instead of fast for database shutdown

--- image/db/rhel/scripts/docker-entrypoint.sh
+++ image/db/rhel/scripts/docker-entrypoint.sh
@@ -279,7 +279,8 @@ docker_temp_server_start() {
 # stop postgresql server after done setting up user and running scripts
 docker_temp_server_stop() {
 	PGUSER="${PGUSER:-postgres}" \
-	pg_ctl -D "$PGDATA" -m fast -w stop
+	PGTIMEOUT="${PGTIMEOUT:-180}" \
+	pg_ctl -D "$PGDATA" -m fast -w stop -t "$PGTIMEOUT"
 }
 
 # check arguments for an option that would cause postgres to stop
