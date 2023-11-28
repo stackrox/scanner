25f2a1b5e46797fcbbf057127ec56376a32d93ef
Kyle Lape <klape@redhat.com>
2023-11-27 20:35:18 -0600

    Add RHTAP build pipeline
    A few differences from the standard RHTAP build pipeline:
    
    * Use of 6GB buildah image to avoid OOMs during container build task
    * There is an extra step to fetch the vuln feed data.  This is done
      outside of the build step to make hermetic builds easier.
    
    The vuln feed data script pulls the definitions from the Google storage
    location and writes them to the "source" folder of the "source"
    workspace which is shared by the build-container task.  This task will
    build using `image/scanner/rhtap/Dockerfile`, which expects the vuln
    files to be in the buildah working directory.
    
    I decided to create a separate dockerfile for RHTAP to avoid any
    regressions with modifying the existing ones.  The dockerfile was
    created using a combination of the upstream and downstream dockerfiles.
    
    Current RHTAP build trigger config:
    
    * PRs will only build in RHTAP when "rhtap" is in the branch name
    * Pushes to master will trigger an RHTAP build

---
+++ scripts/ci/jobs/check-generated.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+# shellcheck source=../../../scripts/ci/lib.sh
+source "$ROOT/scripts/ci/lib.sh"
+
+set -euo pipefail
+
+FAIL_FLAG="/tmp/fail"
+
+info 'Check .containerignore file is in sync with .dockerignore (If this fails, follow instructions in .containerignore to update it.)'
+function check-containerignore-is-in-sync() {
+    diff \
+        --unified \
+        --ignore-blank-lines \
+        <(grep -v -e '^#' .containerignore) \
+        <(grep -vF -e '/.git/' -e '/image/' -e '/bin/' .dockerignore) \
+    > diff.txt
+}
+check-containerignore-is-in-sync || {
+    info ".containerignore file is not in sync with .dockerignore"
+    info "$(cat diff.txt)"
+    git reset --hard HEAD
+    echo check-containerignore-is-in-sync >> "$FAIL_FLAG"
+}
+
+if [[ -e "$FAIL_FLAG" ]]; then
+    echo "ERROR: Some generated file checks failed:"
+    cat "$FAIL_FLAG"
+    exit 1
+fi
