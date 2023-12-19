03b59f6e0775284010476069fe2e850d02386ac1
Ross Tannenbaum <rtannenb@redhat.com>
2023-12-18 17:42:17 -0800

    chore: renew certs (#1354)

--- scripts/cert/gen-cert.sh
+++ scripts/cert/gen-cert.sh
@@ -11,12 +11,12 @@ SCANNER_DB_TLS_FILE=$2
 
 echo "Generating CA for Scanner and Scanner DB"
 cfssl genkey -initca "$ROOT/scripts/cert/csr.json" | cfssljson -bare ca
-SCANNER_CA=$(base64 -in ca.pem)
+SCANNER_CA=$(base64 -i ca.pem)
 
 echo "Generating Cert/Key pair for Scanner"
 cfssl gencert -ca ca.pem -ca-key ca-key.pem -hostname scanner.stackrox "$ROOT/scripts/cert/csr.json" | cfssljson -bare
-SCANNER_CERT=$(base64 -in cert.pem)
-SCANNER_KEY=$(base64 -in cert-key.pem)
+SCANNER_CERT=$(base64 -i cert.pem)
+SCANNER_KEY=$(base64 -i cert-key.pem)
 yq eval ".data[\"ca.pem\"] = \"${SCANNER_CA}\"" "${SCANNER_TLS_FILE}" > tmp.yaml
 yq eval ".data[\"cert.pem\"] = \"${SCANNER_CERT}\"" tmp.yaml > tmp2.yaml
 yq eval ".data[\"key.pem\"] = \"${SCANNER_KEY}\"" tmp2.yaml > tmp3.yaml
@@ -26,8 +26,8 @@ rm *.yaml
 
 echo "Generating Cert/Key pair for Scanner DB"
 cfssl gencert -ca ca.pem -ca-key ca-key.pem -hostname scanner-db.stackrox "$ROOT/scripts/cert/csr.json" | cfssljson -bare
-SCANNER_DB_CERT=$(base64 -in cert.pem)
-SCANNER_DB_KEY=$(base64 -in cert-key.pem)
+SCANNER_DB_CERT=$(base64 -i cert.pem)
+SCANNER_DB_KEY=$(base64 -i cert-key.pem)
 yq eval ".data[\"ca.pem\"] = \"${SCANNER_CA}\"" "${SCANNER_DB_TLS_FILE}" > tmp.yaml
 yq eval ".data[\"cert.pem\"] = \"${SCANNER_DB_CERT}\"" tmp.yaml > tmp2.yaml
 yq eval ".data[\"key.pem\"] = \"${SCANNER_DB_KEY}\"" tmp2.yaml > tmp3.yaml
