aff4a23b42140268961463db904b52a373471fbc
Kyle Lape <klape@redhat.com>
2024-02-01 21:20:14 -0600

    Addressing PR comments

--- scripts/konflux/fetch-vuln-feed-data.sh
+++ scripts/konflux/fetch-vuln-feed-data.sh
@@ -7,7 +7,7 @@ if [[ "$#" < 1 ]]; then
   exit 1
 fi
 
-TARGET_DIR=$1
+TARGET_DIR="$1"
 
 if [[ ! -d "$TARGET_DIR" ]]; then
   echo "$TARGET_DIR is not a valid directory"
@@ -23,6 +23,7 @@ blobs=(
 
 for blob in "${blobs[@]}"; do
   echo "Downloading https://storage.googleapis.com/definitions.stackrox.io/scanner-data/latest/${blob} > $TARGET_DIR/blob-${blob}"
+  # TODO(ROX-22130): Assign proper suffix for tagged commits instead of /latest/.
   curl --fail -s --show-error --retry 4 --retry-max-time 30 --retry-connrefused \
     --output "$TARGET_DIR/blob-${blob}" \
     "https://storage.googleapis.com/definitions.stackrox.io/scanner-data/latest/${blob}"
--- scripts/konflux/version.sh
+++ scripts/konflux/version.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script is used by the Konflux dockerfile to get the correct version to
 # compile in to the binary.  If HEAD points to a git tag, use that; otherwise
 # use `git describe ...`.
+
+set -euo pipefail
 
 if [[ -n "$(git tag --contains)" ]]; then
     git tag --sort=creatordate --contains | tail -1
