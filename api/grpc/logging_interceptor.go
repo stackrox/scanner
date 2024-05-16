debcf31f47153a256d5118ca3dbab1b42a2292db
Ross Tannenbaum <rtannenb@redhat.com>
2024-05-16 16:40:07 -0700

    chore: stop logging pings (#1511)

--- api/grpc/logging_interceptor.go
+++ api/grpc/logging_interceptor.go
@@ -9,8 +9,15 @@ import (
 	"google.golang.org/grpc"
 )
 
+const pingMethod = `/scannerV1.PingService/Ping`
+
 func loggingUnaryServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		if info.FullMethod == pingMethod {
+			// Don't bother logging pings.
+			return handler(ctx, req)
+		}
+
 		logrus.WithFields(map[string]interface{}{
 			"URI": info.FullMethod,
 		}).Infof("Received gRPC request")
