83336e76ec67c3f4b252ca0b756d70b63a09ec0a
Yi Li <yli3@redhat.com>
2024-05-16 17:13:11 -0500

    ROX-23123: Verifying File Integrity while updating offline bundle  (#1497)

--- scripts/ci/jobs/diff-dumps.sh
+++ scripts/ci/jobs/diff-dumps.sh
@@ -122,14 +122,11 @@ upload_offline_dump() {
 
     file_to_check="scanner-v4-defs-${latest_version}.zip"
 
-    if curl --silent --show-error --max-time 60 --retry 3 -o $file_to_check https://definitions.stackrox.io/v4/offline-bundles/$file_to_check; then
-        # If the file exists, add it to scanner-vuln-updates.zip
-        zip scanner-vuln-updates.zip "$file_to_check"
-        echo "$file_to_check added to scanner-vuln-updates.zip"
-    else
-        echo "$file_to_check does not exist."
-        exit 1
-    fi
+    curl --silent --show-error --fail --max-time 60 --retry 3 -o $file_to_check https://definitions.stackrox.io/v4/offline-bundles/$file_to_check
+    unzip -l "$file_to_check"
+    zip scanner-vuln-updates.zip "$file_to_check"
+    echo "$file_to_check added to scanner-vuln-updates.zip"
+
     "${cmd[@]}" gsutil cp scanner-vuln-updates.zip gs://scanner-support-public/offline/v1/scanner-vuln-updates.zip
 }
 
