71cbd4bfd96a3971024f408cbe6c43b7f392a7cd
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-12-18 19:27:05 -0600

    fix(ci): NVD data and test issues (#1737)

--- e2etests/sanity_test.go
+++ e2etests/sanity_test.go
@@ -20,18 +20,40 @@ func checkMatch(t *testing.T, source string, expectedVuln, matchingVuln v1.Vulne
 	if expectedVuln.Metadata == nil {
 		assert.Nil(t, matchingVuln.Metadata, "Expected no metadata for %s but got some", expectedVuln.Name)
 	} else {
-		for _, keys := range [][]string{
+		keySets := [][]string{
 			{source, "CVSSv2", "ExploitabilityScore"},
 			{source, "CVSSv2", "Score"},
 			{source, "CVSSv2", "ImpactScore"},
 			{source, "CVSSv2", "Vectors"},
-			{source, "CVSSv3", "ExploitabilityScore"},
-			{source, "CVSSv3", "Score"},
-			{source, "CVSSv3", "ImpactScore"},
-			{source, "CVSSv3", "Vectors"},
-		} {
-			assert.NotNil(t, deepGet(expectedVuln.Metadata, keys...), "Value for nil for %+v", keys)
-			assert.Equal(t, deepGet(expectedVuln.Metadata, keys...), deepGet(matchingVuln.Metadata, keys...), "Failed for %+v", keys)
+		}
+
+		testHasCVSSv3 := deepGet(expectedVuln.Metadata, []string{source, "CVSSv3"}...) != nil
+
+		vulnCVSSv3 := deepGet(matchingVuln.Metadata, []string{source, "CVSSv3"}...)
+		vulnCVSSv3Vectors := deepGet(matchingVuln.Metadata, []string{source, "CVSSv3", "Vectors"}...)
+		// Assume that CVSSv3 scores exist if Vectors is not empty.
+		vulnHasCVSSv3 := vulnCVSSv3Vectors != nil && vulnCVSSv3Vectors.(string) != ""
+
+		// If the test case does not have CVSSv3 scores but the matching vuln does, fail the test.
+		// This was added when NVD stopped returning CVSSv3 data for some vulns which we had
+		// test cases for, this condition ensures we are alerted when/if the data returns.
+		assert.False(t, !testHasCVSSv3 && vulnHasCVSSv3, "Test case for %q is missing CVSSv3 scores, please add the scores to the test case. Scores from vuln data: %+v", expectedVuln.Name, vulnCVSSv3)
+
+		// Compare CVSSv3 data only when it exists in the test case.
+		if testHasCVSSv3 {
+			keySets = append(keySets, [][]string{
+				{source, "CVSSv3", "ExploitabilityScore"},
+				{source, "CVSSv3", "Score"},
+				{source, "CVSSv3", "ImpactScore"},
+				{source, "CVSSv3", "Vectors"},
+			}...)
+		} else {
+			t.Logf("WARN: No CVSSv3 data provided for %q, skipping CVSSv3 field validation.", expectedVuln.Name)
+		}
+
+		for _, keys := range keySets {
+			assert.NotNil(t, deepGet(expectedVuln.Metadata, keys...), "Value for nil for %+v in vuln %q", keys, expectedVuln.Name)
+			assert.Equal(t, deepGet(expectedVuln.Metadata, keys...), deepGet(matchingVuln.Metadata, keys...), "Failed for %+v in vuln %q", keys, expectedVuln.Name)
 		}
 	}
 	expectedVuln.Metadata = nil
