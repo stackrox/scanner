3647454bd8fcb801c375502ab2dd0d1d55210a27
Ross Tannenbaum <rtannenb@redhat.com>
2025-07-30 10:58:47 -0700

    e2e: fix tests with latest vuln data (#1999)

--- cmd/clair/main.go
+++ cmd/clair/main.go
@@ -114,7 +114,8 @@ func Boot(config *Config, slimMode bool) {
 	go func() {
 		defer wg.Add(-1)
 		var err error
-		db, err = database.OpenWithRetries(config.Database, true, 30, 10*time.Second)
+		// Wait for the DB to be ready: 10 minutes.
+		db, err = database.OpenWithRetries(config.Database, true, 60, 10*time.Second)
 		if err != nil {
 			log.WithError(err).Fatal("Failed to open database despite multiple retries...")
 		}
