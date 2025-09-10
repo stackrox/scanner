7062e9f471cc622fbc0a2648cea9c26d6b141a84
David Caravello <119438707+dcaravel@users.noreply.github.com>
2025-09-10 14:56:40 -0500

    Backport s390x and CI fixes - 2.35/4.6 (#2161)
    Co-authored-by: Brad Lugo <blugo@redhat.com>

--- scripts/ci/deploy.sh
+++ scripts/ci/deploy.sh
@@ -32,7 +32,7 @@ _wait_for_scanner() {
     kubectl -n stackrox get pod
     POD="$(kubectl -n stackrox get pod -o jsonpath='{.items[?(@.metadata.labels.app=="scanner")].metadata.name}')"
     [[ -n "${POD}" ]]
-    kubectl -n stackrox wait "--for=condition=Ready" "pod/${POD}" --timeout=10m
+    kubectl -n stackrox wait "--for=condition=Ready" "pod/${POD}" --timeout=30m
     kubectl -n stackrox get pod
 }
 
