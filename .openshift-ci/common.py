17cb5ae8ea94880434c269f118a5a3f831ded1cc
Brad Lugo <blugo@redhat.com>
2026-04-06 08:21:11 -0700

    ROX-33852: Update OpenShift CI scripts (#3036)
    Co-authored-by: David Caravello <119438707+dcaravel@users.noreply.github.com>

--- .openshift-ci/common.py
+++ .openshift-ci/common.py
@@ -1,14 +1,25 @@
+from datetime import datetime
 import subprocess
 
 '''
-Copied from https://github.com/stackrox/stackrox/blob/master/.openshift-ci/common.py
+Adapted from https://github.com/stackrox/stackrox/blob/master/.openshift-ci/common.py
 '''
 
 def popen_graceful_kill(cmd):
+    log_print(f"Sending SIGTERM to {cmd.args}")
     cmd.terminate()
     try:
         cmd.wait(5)
+        log_print("Terminated")
     except subprocess.TimeoutExpired as err:
+        log_print(f"Exception raised waiting after SIGTERM to {cmd.args}, {err}")
+        # SIGKILL if necessary
+        log_print(f"Sending SIGKILL to {cmd.args}")
         cmd.kill()
         cmd.wait(5)
-        raise err
+        log_print("Terminated")
+
+def log_print(*args):
+    now = datetime.now()
+    time = now.strftime("%H:%M:%S")
+    print(f"{time}:", *args)
